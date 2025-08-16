d53f007f05d16fae98f13054434ec64072b0063c
Robinson Steven Nuñez Portela <robinson.nunez-p@mail.escuelaing.edu.co>
2025-08-16 16:51:27 -0500

    Carrera con lambda minimo

--- Pareja_Caballero_Portela/reto2/CarreraParalela.java
+++ Pareja_Caballero_Portela/reto2/CarreraParalela.java
@@ -1,5 +1,18 @@
+import java.util.List;
+import java.util.function.Function;
+
 public class CarreraParalela {
+
+    static Function<List<Integer>, Integer> getMin = list -> list.stream().min(Integer::compare).orElse(null);
+
+    static Function<List<Integer>, Integer>  getSize = list -> list.size();
+
     public static void main(String[] args) {
-        System.out.println("Carrera Paralela, punto inicial");
+    List<Integer> numbers = List.of(3, 17, 2, 9, 25);
+    Integer min = getMin.apply(numbers);
+    Integer size = getSize.apply(numbers);
+    System.out.println("List: " + numbers);
+    System.out.println("Minimum number: " + min);
+    System.out.println("Size of numbers: " + size);
     }
-}
+} 
