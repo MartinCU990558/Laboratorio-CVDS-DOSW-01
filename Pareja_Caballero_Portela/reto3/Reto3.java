11819f54c91d4dc450fb144640f8d968693cb78b
JuanCaballero9778 <juan.ccastellanos@mail.escuelaing.edu.co>
2025-08-17 16:30:21 -0500

    Mensaje invertido Buffer

---
+++ Pareja_Caballero_Portela/reto3/Reto3.java
@@ -0,0 +1,18 @@
+import java.util.function.Function;
+public class Reto3{
+
+    public static String ecoMisterious(String message){
+        StringBuffer buffer = new StringBuffer(message); // Invertir usando buffer
+        return buffer.reverse().toString();
+    }
+
+    public static void main (String[] args){
+
+        Function<String, String> fun = msg -> Reto3.ecoMisterious(msg);
+        
+        String message = "Ciclos";
+        String result = fun.apply(message);
+
+        System.out.println("Reverse message: " + result);
+    }
+}
