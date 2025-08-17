ba0e3c3fda54092c3b2b6e34ceb25f44e6ac06d1
Karol2905 <karol.estupinan-v@mail.escuelaing.edu.co>
2025-08-17 12:04:43 -0500

    Reto 1 completado

--- Idarraga_Estupinan/Reto1/Mensaje.java
+++ Idarraga_Estupinan/Reto1/Mensaje.java
@@ -5,13 +5,10 @@ import java.util.*;
 public class Mensaje {
 
     public static String generarMensaje(Estudiante estudiante, Estudiante estudiante1) {
-        return "!Hola Bienvenidos! nosotros somos la pareja conformada por " + estudiante.getNombre() + "estudiante de la escuela de " + estudiante.getSemestre() +
-                " de " + estudiante.getEdad() + " años" + 
-        estudiante1.getNombre() +
-
-                "nuestra edad es: " + estudiante.getEdad() + " "+estudiante1.getEdad() +
-                ", nuestros correos son: " + estudiante.getCorreo() +" "+ estudiante1.getCorreo() +
-                ", estamos en los Semestres: " + estudiante.getSemestre() + " "+estudiante1.getSemestre();
+        return "!Hola Bienvenidos! nosotros somos la pareja conformada por " + estudiante.getNombre() + " estudiante de la escuela de " + estudiante.getSemestre() +" semestre "+
+                " de " + estudiante.getEdad() + " años y " + estudiante1.getNombre() + " estudiante de la escuela de "+estudiante1.getSemestre() +
+                " semestre de " + estudiante1.getEdad() +" años, nuestros correos institucionales son "+ estudiante.getCorreo() +" y "+
+                estudiante1.getCorreo();
     }
 
 }
