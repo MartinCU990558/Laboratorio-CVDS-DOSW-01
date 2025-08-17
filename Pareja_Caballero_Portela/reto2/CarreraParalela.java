30df80b49b92ae08f9ed1849b05702a28fd65a41
Robinson Steven Nuñez Portela <robinson.nunez-p@mail.escuelaing.edu.co>
2025-08-16 19:07:19 -0500

    Segunda  vuelta carril 2, funcion ternaria de divisor 2 para maximo numero

--- Pareja_Caballero_Portela/reto2/CarreraParalela.java
+++ Pareja_Caballero_Portela/reto2/CarreraParalela.java
@@ -1,18 +1,31 @@
 import java.util.List;
 import java.util.function.Function;
 
+
 public class CarreraParalela {
 
-    static Function<List<Integer>, Integer> getMin = list -> list.stream().min(Integer::compare).orElse(null);
+    static Function<List<Integer>, Integer> getMin = list -> list.stream().min(Integer::compare).orElse(null); //Primer choque carril 2.
 
-    static Function<List<Integer>, Integer>  getSize = list -> list.size();
+    static Function<List<Integer>, Integer>  getSize = list -> list.size();  //Primer choque carril 2.
+
+    //Segunda Vuelta Choque de Carriles - Comprobarndo si el numero maximo es divisor de 2
+
+    static Function<List<Integer>, Integer> getMax = list -> list.stream().max(Integer::compare).orElse(null); 
+
+    static Function<Integer, String> isDivisor = num -> (num % 2 == 0) ? "divisible" : "No divisible"; 
+
 
     public static void main(String[] args) {
     List<Integer> numbers = List.of(3, 17, 2, 9, 25);
-    Integer min = getMin.apply(numbers);
-    Integer size = getSize.apply(numbers);
-    System.out.println("List: " + numbers);
-    System.out.println("Minimum number: " + min);
-    System.out.println("Size of numbers: " + size);
+    int min = getMin.apply(numbers); //Primer choque carril 2.
+    int size = getSize.apply(numbers); //Primer choque carril 1.
+    int max = getMax.apply(numbers); //Segunda  vuelba choque carril 1
+    String div = isDivisor.apply(max); //Segunda  vuelba choque carril 1
+
+    System.out.println("List: " + numbers);  //Primer choque carril 2.
+    System.out.println("Minimum number: " + min); //Primer choque carril 1.
+    System.out.println("Size of numbers: " + size); //Primer choque carril 1
+    System.out.println("Maximum number" + max); //Segunda  vuelba choque carril 1
+    System.out.println("The maximum number (" + max + ") is: " + div); // Segunda vueltac hoque carril 1
     }
 } 
