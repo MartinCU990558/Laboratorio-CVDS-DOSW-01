e821a91ecc35f4a3577ad1c4afb96ebdb1b4f0af
Robinson677 <robinson.nunez-p@mail.escuelaing.edu.co>
2025-08-17 16:27:51 -0500

    Concatenacion con un espacio reto3 estudiante A

---
+++ Pareja_Caballero_Portela/reto3/Reto3.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.stream.IntStream;
+
+
+public class Reto3{
+
+    public static String ecoMisterious(String message){
+        StringBuilder sb = new StringBuilder();
+        IntStream.range(0, 3).forEach(i -> sb.append(message).append(" "));
+        return sb.toString().trim();
+    }
+
+
+
+    public static void main(String[] args){
+        String message = "Ciclos";
+        System.out.println("Eco Misterious Contenated: " + ecoMisterious(message));
+
+    }
+
+}
