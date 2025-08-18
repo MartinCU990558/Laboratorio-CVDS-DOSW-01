c41dd49ebb36347a95284954343c14f89dcdcbc4
Tulio <tuliojr3101@gmail.com>
2025-08-18 15:01:41 -0500

    Subiendo la funcion del gran choque combinacion

--- Pareja_Nieto_Riano/reto3/Reto3.java
+++ Pareja_Nieto_Riano/reto3/Reto3.java
@@ -1,3 +1,4 @@
+import java.util.function.Function;
 import java.util.stream.IntStream;
 
 public class Reto3 {
@@ -13,13 +14,22 @@ public class Reto3 {
     }
 
     static StringBuffer invertir(String message) {
-    StringBuffer sbf = new StringBuffer(message);
-    return sbf.reverse();
+        StringBuffer sbf = new StringBuffer(message);
+        return sbf.reverse();
     }
 
+    static void combinacion(String message) {
+        Function<String, StringBuilder> sb = (x) -> Reto3.repeticion(x);
+        String s = sb.apply(message).toString();
+        System.out.println(s);
+        Function<String, StringBuffer> sbf = (x) -> Reto3.invertir(x);
+        String invertido = sbf.apply(s).toString();
+        System.out.println(invertido);
+    }
 
     public static void main(String args[]) {
         System.out.println(repeticion("hola"));
         System.out.println(invertir("hola"));
+        combinacion("Hola");
     }
 }
