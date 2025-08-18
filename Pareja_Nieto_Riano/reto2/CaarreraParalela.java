f15e41634e201e955773a758c40e96905042fdb6
JuanPablo990 <juan.nieto-co@mail.escuelaing.edu.co>
2025-08-17 19:55:50 -0500

    implementando nueva funcionalidad

---
+++ Pareja_Nieto_Riano/reto2/CaarreraParalela.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.Arrays;
+
+public class CaarreraParalela {
+
+    static int biggerNumber(List<Integer> numbers) {
+        return numbers.stream().max(Integer::compare).get();
+    }
+
+    static int minimumNumber(List<Integer> numbers) {
+        return numbers.stream().min(Integer::compare).get();
+    }
+
+    static Long amount(List<Integer> numbers) {
+        return numbers.stream().count();
+    }
+
+    static GeneralObject fusion(List<Integer> numbers) {
+        GeneralObject object = new GeneralObject(minimumNumber(numbers), biggerNumber(numbers), amount(numbers));
+        return object;
+    }
+
+    public static void main(String args[]) {
+        List<Integer> numbers = Arrays.asList(11, 8, 9, 15, 39, 1, 4, 83);
+        System.out.println(biggerNumber(numbers));
+        System.out.println(minimumNumber(numbers));
+        System.out.println(amount(numbers));
+    }
+}
