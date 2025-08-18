3c5634e4f0b894e903995463f506feeeff1be912
JuanPablo990 <juan.nieto-co@mail.escuelaing.edu.co>
2025-08-18 15:51:31 -0500

    se agrego la funcionalidad del hastable

--- Pareja_Nieto_Riano/reto4/Reto4.java
+++ Pareja_Nieto_Riano/reto4/Reto4.java
@@ -3,12 +3,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.Hashtable;
 
 public class Reto4 {
 
     public static HashMap<String, Integer> almacen(List<Map.Entry<String, Integer>> numbers) {
         HashMap<String, Integer> mapa = numbers.stream()
                 .collect(Collectors.toMap(k -> k.getKey(), v -> v.getValue(), (y, nuevo) -> y, HashMap::new));
+        return mapa;
+    }
+
+    public static Hashtable<String, Integer> almacenSeguro(List<Map.Entry<String, Integer>> numbers) {
+        Hashtable<String, Integer> mapa = numbers.stream()
+                .collect(Collectors.toMap(
+                        k -> k.getKey(),
+                        v -> v.getValue(),
+                        (y, nuevo) -> y, // conserva el primero
+                        Hashtable::new));
         return mapa;
     }
 
@@ -19,6 +30,7 @@ public class Reto4 {
                 new AbstractMap.SimpleEntry<>("tres", 3));
 
         System.out.println(almacen(numbers));
+        System.out.println(almacenSeguro(numbers));
 
     }
 }
