5612868fbf2c3bc249bbbf89ec284b9e4f3126a8
JuanPablo990 <juan.nieto-co@mail.escuelaing.edu.co>
2025-08-17 19:34:40 -0500

    por el carril numero dos se realizo  la busqueda del  minimo y su cantidad

--- Pareja_Nieto_Riano/reto2/CaarreraParalela.java
+++ Pareja_Nieto_Riano/reto2/CaarreraParalela.java
@@ -6,9 +6,19 @@ public class CaarreraParalela {
     static int biggerNumber(List<Integer> numbers) {
         return numbers.stream().max(Integer::compare).get();
     }
+    
+    static int MinimunNumber(List<Integer> numbers) {
+        return numbers.stream().min(Integer::compare).get();
+    }
+
+    static Long amount(List<Integer> numbers) {
+        return numbers.stream().count();
+    }
 
     public static void main(String args[]) {
         List<Integer> numbers = Arrays.asList(11, 8, 9, 15, 39, 1, 4, 83);
         System.out.println(biggerNumber(numbers));
+        System.out.println(MinimunNumber(numbers));
+        System.out.println(amount(numbers));
     }
 }
