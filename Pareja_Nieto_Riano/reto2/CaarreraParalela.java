7f4ea652e550131ded4ea0c525c7c2ad609bc027
Tulio <tuliojr3101@gmail.com>
2025-08-17 20:56:37 -0500

    La gran meta

--- Pareja_Nieto_Riano/reto2/CaarreraParalela.java
+++ Pareja_Nieto_Riano/reto2/CaarreraParalela.java
@@ -26,11 +26,33 @@ public class CaarreraParalela {
         return object;
     }
 
+    static Resultados fusionar(List<Integer> numbers, List<Integer> numbersTwo) {
+        int maximum = biggerNumber(numbers) > biggerNumber(numbersTwo) ? biggerNumber(numbers)
+                : biggerNumber(numbersTwo);
+        int minimum = minimumNumber(numbers) > minimumNumber(numbersTwo) ? biggerNumber(numbersTwo)
+                : biggerNumber(numbers);
+        Long amountNumbers = amount(numbers);
+        Long amountNumbersTwo = amount(numbersTwo);
+        boolean multipleOfTwo = maximum % 2 == 0 ? true : false;
+        boolean divOfTwo = 2 % maximum == 0 ? true : false;
+        boolean even = amountNumbers % 2 == 0 ? true : false;
+        boolean eventwo = amountNumbersTwo % 2 == 0 ? true : false;
+        boolean odd = amountNumbers % 2 != 0 ? true : false;
+        boolean oddtwo = amountNumbersTwo % 2 != 0 ? true : false;
+
+        Resultados result = new Resultados(maximum, minimum, amountNumbers, amountNumbersTwo, multipleOfTwo, divOfTwo,
+                even, eventwo, odd, oddtwo);
+
+        return result;
+    }
+
     public static void main(String args[]) {
         List<Integer> numbers = Arrays.asList(11, 8, 9, 15, 39, 1, 4, 83);
+        List<Integer> numberstwo = Arrays.asList(15, 41, 923, 13, 4, 1, 2, 0);
         System.out.println(biggerNumber(numbers));
         System.out.println(minimumNumber(numbers));
         System.out.println(amount(numbers));
         System.out.println(fusion(numbers));
+        System.out.println(fusionar(numbers,numberstwo));
     }
 }
