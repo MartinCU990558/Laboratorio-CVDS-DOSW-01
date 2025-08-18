9fb373fa7671032a8f2d33f55fc7b90b1fead6b1
JuanCaballero9778 <juan.ccastellanos@mail.escuelaing.edu.co>
2025-08-17 20:04:16 -0500

    Función que combina dos mapas estudiante 2

--- Pareja_Caballero_Portela/reto4/Reto4.java
+++ Pareja_Caballero_Portela/reto4/Reto4.java
@@ -1,8 +1,15 @@
 import java.util.AbstractMap;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
 public class Reto4 {
+    /**
+     * Método que crea un hash table.
+     * @param list
+     * @return
+     */
     public static Hashtable <String, Integer> createHashTable(List<AbstractMap.SimpleEntry<String, Integer>> list){
         Hashtable<String, Integer> table = new Hashtable<>();
 
@@ -10,6 +17,22 @@ public class Reto4 {
             table.putIfAbsent(even.getKey(), even.getValue());
         }
         return table;
+    }
+
+    /**
+     * Método que combina ambos hash.
+     * @param map
+     * @param table
+     * @return
+     */
+    public static Map<String, Integer> spliHash(Map<String, Integer> hashMap, Map<String, Integer> hashTable) {
+        Map<String, Integer> combined = new Hashtable<>(hashTable);
+
+        for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+            combined.putIfAbsent(entry.getKey(), entry.getValue());
+        }
+
+        return combined;
     }
 
 
@@ -23,8 +46,11 @@ public class Reto4 {
         );
 
         Hashtable<String, Integer> table = createHashTable(listTable);
+        
+        Map<String, Integer> combined = splitHash(map, table);
+
         System.out.println("Hashtable: " + table);
-     
+        System.out.println("Combined: " + combined);
     }
 
 }
