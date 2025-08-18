5b90bd7f8ee5a60c7cb8aeb4698f4a047b2fe315
oscar sanchez <oscar.sporras@mail.escuelaing.edu.co>
2025-08-18 00:03:31 -0500

    Lista HashSet

--- Pareja_Sanchez_Suarez/reto5/Reto5.java
+++ Pareja_Sanchez_Suarez/reto5/Reto5.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Reto5 {
     public static void main(String[] args) {
+     Random random = new Random();
+        List<Integer> numerosAleatorios = Stream
+                .generate(() -> random.nextInt(50))
+                .limit(10)
+                .toList();
+
+        List<Integer> hashSet = numerosAleatorios.stream()
+                .filter(n -> n % 3 != 0)
+                .collect(Collectors.toUnmodifiableList());
+        System.out.println("HashSet:" + hashSet);
     }
 }
