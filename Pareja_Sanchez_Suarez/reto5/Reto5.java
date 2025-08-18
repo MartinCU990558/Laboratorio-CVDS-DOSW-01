014def3f47cf7c3737dbdb5444db4d0f381e2c6f
oscar sanchez <oscar.sporras@mail.escuelaing.edu.co>
2025-08-18 18:30:03 -0500

    Implementacion lista TreeSet

--- Pareja_Sanchez_Suarez/reto5/Reto5.java
+++ Pareja_Sanchez_Suarez/reto5/Reto5.java
@@ -1,4 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Reto5 {
     public static void main(String[] args) {
+        Random random = new Random();
+        List<Integer> numerosAleatorios = Stream
+                .generate(() -> random.nextInt(50))
+                .limit(10)
+                .sorted()
+                .toList();
+
+        List<Integer> hashSet = numerosAleatorios.stream()
+                .filter(n -> n % 5 != 0)
+                .collect(Collectors.toUnmodifiableList());
+        System.out.println("HashSet:" + hashSet);
     }
 }
