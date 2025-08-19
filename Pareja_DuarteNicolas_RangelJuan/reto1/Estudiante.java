0a048a698c8676d919bfe1c92dd51af41b4353e3
Nicolas Duarte <nicolas.duarte-r@mail.escuelaing.edu.cogit satatusgit statusgit commit -m finish>
2025-08-19 11:46:03 -0500

    cambio de rama

--- Pareja_DuarteNicolas_RangelJuan/reto1/Estudiante.java
+++ Pareja_DuarteNicolas_RangelJuan/reto1/Estudiante.java
@@ -30,4 +30,13 @@ public class Estudiante {
     public String getApellido() {
         return apellido;
     }
+    public static void main(String[] args) {
+        List<Estudiante> pareja = Arrays.asList(
+                new Estudiante("Nicolas", "Duarte", 21, "nicolas.duarte@mail.escuelaing.edu.co", 6),
+                new Estudiante("Juan", "Rangel", 22, "juan.rangel@mail.escuelaing.edu.co", 5)
+        );
+
+        String mensaje = Mensaje.generar(pareja);
+        System.out.println(mensaje);
+    }
 }
