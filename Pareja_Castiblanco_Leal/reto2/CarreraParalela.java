b0704250125ec9fc8b1c5052d92460f2e76fc033
unknown <julian.castiblanco-r@labinfo.is.escuelaing.edu.co>
2025-08-19 12:32:02 -0500

    feature: numero min divisible

--- Pareja_Castiblanco_Leal/reto2/CarreraParalela.java
+++ Pareja_Castiblanco_Leal/reto2/CarreraParalela.java
@@ -9,7 +9,7 @@ public class CarreraParalela {
     public static void main(String[] args) {
         List<Integer> numberList = new ArrayList<>();
         Random rand = new Random();
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < rand.nextInt(50); i++){
             numberList.add(rand.nextInt(50));
         }
         System.out.println("La lista es: "+numberList);
@@ -17,5 +17,7 @@ public class CarreraParalela {
         Optional<Integer> minNum = numberList.stream().min((a, b) -> a.compareTo(b));
         Integer length = numberList.size();
         System.out.println("El número más pequeño es: "+minNum.get()+" y la longitud de lista es: "+length);
+        String mensaje = (minNum.get() % 2 == 0 && minNum.get() > 0) ? "El número menor es divisor de 2" : "El número menor no es divisor de 2";
+        System.out.println(mensaje);
     }
 }
