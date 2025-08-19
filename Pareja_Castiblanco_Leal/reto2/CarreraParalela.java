6ea2d6ddfef0e5b473dbb2d6d4e64429c9eb434e
unknown <juan.lcruz@labinfo.is.escuelaing.edu.co>
2025-08-19 12:24:49 -0500

    feature:Añadidura del if ternario para verificar si el múltiplo de dos en el carril 1

--- Pareja_Castiblanco_Leal/reto2/CarreraParalela.java
+++ Pareja_Castiblanco_Leal/reto2/CarreraParalela.java
@@ -9,12 +9,14 @@ public class CarreraParalela {
     public static void main(String[] args) {
         List<Integer> numberList = new ArrayList<>();
         Random rand = new Random();
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < rand.nextInt(50); i++){
             numberList.add(rand.nextInt(50));
         }
         System.out.println("La lista es: "+numberList);
 
         Optional<Integer> maxNum = numberList.stream().max((a, b) -> a.compareTo(b));
         System.out.println("El número más grande es: "+maxNum.get());
+        String mensaje = (maxNum.get() % 2 == 0) ? "El número mayor es múltiplo de 2" : "El número mayor no es múltiplo de 2";
+        System.out.println(mensaje);
     }
 }
