43094f65a3d49288b5bb00c8f4678ef2e406473e
oscar sanchez <oscar.sporras@mail.escuelaing.edu.co>
2025-08-18 19:45:09 -0500

    Reto#5 culminado

--- Pareja_Sanchez_Suarez/reto5/Reto5.java
+++ Pareja_Sanchez_Suarez/reto5/Reto5.java
@@ -10,20 +10,24 @@ import java.util.stream.Stream;
 public class Reto5 {
     public static void main(String[] args) {
      Random random = new Random();
-        List<Integer> numerosAleatorios = Stream
+        List<Integer> numerosAleatorios1 = Stream
+                .generate(() -> random.nextInt(50))
+                .limit(10)
+                .toList();
+        List<Integer> numerosAleatorios2 = Stream
                 .generate(() -> random.nextInt(50))
                 .limit(10)
                 .toList();
 
-        List<Integer> hashSet = numerosAleatorios.stream()
+        List<Integer> hashSet = numerosAleatorios1.stream()
                 .filter(n -> n % 3 != 0)
                 .collect(Collectors.toUnmodifiableList());
-        System.out.println("HashSet:" + hashSet);
+        System.out.println("HashSet:"+numerosAleatorios1 +" -> "+ hashSet);
 
-        List<Integer> treeSet = numerosAleatorios.stream()
+        List<Integer> treeSet = numerosAleatorios2.stream()
                 .filter(n -> n % 5 != 0)
                 .collect(Collectors.toUnmodifiableList());
-        System.out.println("TreeSet:" + treeSet);
+        System.out.println("TreeSet:"+ numerosAleatorios2+ " -> " + treeSet);
         List<Integer> Union = Stream.concat(hashSet.stream(), treeSet.stream())
                 .distinct()
                 .sorted()
