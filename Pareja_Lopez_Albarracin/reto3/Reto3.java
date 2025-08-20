c50fe10a1073124ac6e226a3867210787822337e
Julian <julianl116910@gmail.com>
2025-08-19 22:00:36 -0500

    Rama #3

--- Pareja_Lopez_Albarracin/reto3/Reto3.java
+++ Pareja_Lopez_Albarracin/reto3/Reto3.java
@@ -1,25 +1,5 @@
 package Pareja_Lopez_Albarracin.reto3;
 import java.util.stream.IntStream;
 public class Reto3{
-    public static void main(String[] args){
-        System.out.println(reverseString("Eco"));
-        System.out.println(repeatString("Eco"));
-    }
-
-    public static StringBuffer reverseString(String message){
-        return new StringBuffer(message).reverse();
-    }
-
-    public static StringBuilder repeatString(String message){
-        StringBuilder messageToRepeat = new StringBuilder();
-        IntStream.range(0, 3).forEach(i -> {
-            if (i > 0) messageToRepeat.append("");
-            messageToRepeat.append(message);
-        });
-        return messageToRepeat;
-    }
-
-    public void combination(String message){
-        
-    }
+   
 }
