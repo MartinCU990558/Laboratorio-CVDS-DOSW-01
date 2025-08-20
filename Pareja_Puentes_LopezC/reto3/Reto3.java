8047fa9752f09653a23fcc59c7f5ce58842b6a3b
sebasPuentes <seb123.juli@gmail.com>
2025-08-19 22:41:00 -0500

    Reto 3 finalizado

--- Pareja_Puentes_LopezC/reto3/Reto3.java
+++ Pareja_Puentes_LopezC/reto3/Reto3.java
@@ -1,17 +1,36 @@
 package reto3;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-public class reto3 {
+public class Reto3 {
+
+    
+    public static StringBuilder repeticion(String mensaje) {
+        StringBuilder resultado = new StringBuilder(); 
+        Stream.generate(() -> mensaje)
+            .limit(3).forEach(resultado::append);
+    
+    return resultado;
+    }
+
+    public static StringBuffer reverso(String mensaje){
+        StringBuffer sb = new StringBuffer(mensaje);
+        return sb.reverse();
+    }
+
+    public static String combinacion(String mensaje){
+        String repetido = IntStream.range(0, 3)
+                                   .mapToObj(i -> mensaje)
+                                   .collect(Collectors.joining(" "));
+        return reverso(repetido).toString();
+    }
+
     public static void main(String[] args) {
-        
+        Function<String, String> mensaje = (msg) -> combinacion(msg);
+        String resultado = mensaje.apply("NestorCastor");
+        System.out.println(resultado);
     }
-    public StringBuilder repeticion(String mensaje ){
-         StringBuilder resultado = new StringBuilder();
-        
-        IntStream.range(0, 3)
-                .forEach(i -> resultado.append(mensaje));
-        
-        return resultado;
-    }
-}
 
+}
