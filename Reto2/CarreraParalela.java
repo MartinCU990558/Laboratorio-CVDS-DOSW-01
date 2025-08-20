c047f9037e8b330ab7849e45fb07edba1cc33aef
Juanseom <juanse.om13@gmail.com>
2025-08-20 14:30:07 -0500

    Resolución de conflictos de lineas duplicadas

--- Reto2/CarreraParalela.java
+++ Reto2/CarreraParalela.java
@@ -12,22 +12,14 @@ public class CarreraParalela {
     public static int calcularMinimo(List<Integer> numeros) {
         return numeros.stream().min(Integer::compare).get();
     }
-<<<<<<< HEAD
-    
-    public static ResumenLista analizar(List<Integer> numeros) {
-    
-        
-        int maximo = calcularMaximo(numeros);
-        int minimo = calcularMinimo(numeros);
-        int conteo = numeros.size();
-        
-=======
 
+    
     public static ResumenLista analizar(List<Integer> numeros) {
+    
+        
         int maximo = calcularMaximo(numeros);
         int minimo = calcularMinimo(numeros);
         int conteo = numeros.size();
->>>>>>> 95b6d9f427adb24e1d7ebbfd9845b202b3a5a783
         return new ResumenLista(maximo, minimo, conteo);
     }
 }
