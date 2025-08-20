970f24f5a9e934233d403e51933a1642dfb23baa
sebasPuentes <seb123.juli@gmail.com>
2025-08-19 21:27:53 -0500

    Funcion Buffer realizada

---
+++ Pareja_Puentes_LopezC/reto3/Reto3.java
@@ -0,0 +1,18 @@
+package  reto3;
+
+public class Reto3{
+
+    public static StringBuffer mensajeReverso(String mensaje){
+        StringBuffer sb = new StringBuffer(mensaje);
+        return sb.reverse();
+    }
+
+    public static String combinacion(String mensaje){
+        return mensaje;
+    }
+
+    public static void main(String[] args) {
+        System.err.println("NestorCastor");
+    }
+
+}
