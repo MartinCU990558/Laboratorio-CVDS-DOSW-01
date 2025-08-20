6e310cfe9e767ee984ba2538b61523b58c6e1acf
Julian <julianl116910@gmail.com>
2025-08-19 20:45:39 -0500

    Reto#1 Culminado

---
+++ Pareja_Lopez_Albarracin/reto1/Mensaje.java
@@ -0,0 +1,12 @@
+package Pareja_Lopez_Albarracin.reto1;
+public class Mensaje {
+    private String mensaje;
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+}
