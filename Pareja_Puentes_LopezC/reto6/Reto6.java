855edd4b36778963950c4d02fe08d42707bae2b0
sebasPuentes <seb123.juli@gmail.com>
2025-08-19 23:24:07 -0500

    Fragmento 1 Correcion

--- Pareja_Puentes_LopezC/reto6/Reto6.java
+++ Pareja_Puentes_LopezC/reto6/Reto6.java
@@ -7,25 +7,30 @@ public class Reto6{
     public static void fragmento1(String mensaje){
         switch (mensaje) {
             case "SALUDAR":
-                System.out.println("¡Saludos, viajero del tiempo y del código!");
+                System.out.println("La máquina dice: ¡Saludos, viajero del tiempo y del código!");
                 break;
             case "DESPEDIR":
-                System.out.println("Que los bits te acompañen, hasta la próxima misión.");
+                System.out.println("La máquina dice: 'Que los bits te acompañen, hasta la próxima misión.'");
                 break;
             case "CANTAR":
-                System.out.println("01010101");
+                System.out.println("La máquina canta: '01010101'");
                 break;
             case "DANZAR":
-                System.out.println("Girando en modo fiesta.");
+                System.out.println("La máquina gira y emite chispas: 'Girando en modo fiesta.'");
                 break;
         }
     }
 
     public static void main(String[] args) {
         Map<String, Runnable> map = new HashMap<>();
-        map.put("La maquina dice:", () -> fragmento1("SALUDAR"));
-        map.put("La maquina dice:", () -> fragmento1("DESPEDIR"));
-        map.put("La maquina canta:", () -> fragmento1("CANTAR"));
-        map.put("La maquina gira y emite chispas:", () -> fragmento1("DANZAR"));
+        map.put("SALUDAR", () -> fragmento1("SALUDAR"));
+        map.put("DESPEDIR", () -> fragmento1("DESPEDIR"));
+        map.put("CANTAR", () -> fragmento1("CANTAR"));
+        map.put("DANZAR", () -> fragmento1("DANZAR"));
+
+        map.get("La maquina dice:").run();
+        map.get("La maquina dice:").run();
+        map.get("La maquina canta:").run();
+        map.get("La maquina gira y emite chispas:").run();
     }
 }
