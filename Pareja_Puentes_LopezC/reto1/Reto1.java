dd7d22bbbb1cdbfeedf4d33d16a0f8a61755e8b8
Nestor David <nestor.lopez-c@mail.escuelaing.edu.co>
2025-08-19 22:28:19 -0500

    Reto 1 Finalizado

---
+++ Pareja_Puentes_LopezC/reto1/Reto1.java
@@ -0,0 +1,16 @@
+package reto1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Reto1 {
+    public static void main(String[] args) {
+        List<Estudiante> estudiantes = Arrays.asList(
+            new Estudiante("Juan Puentes", 20, "juan.puentes@mail.escueling.edu.co", 6),
+            new Estudiante("Nestor Lopez", 19, "nestor.lopez@mail.escuelaing.edu.co", 6)
+        );
+        
+        String mensaje = MensajeBienvenida.generarMensaje(estudiantes);
+        System.out.println(mensaje);
+    }
+}
