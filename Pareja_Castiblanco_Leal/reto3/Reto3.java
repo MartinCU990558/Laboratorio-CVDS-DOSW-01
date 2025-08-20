6cbe56dbe2c15bbbf6cc2246f559664a41ce34d2
Juan Leal <lecrujuan@gmail.com>
2025-08-19 22:27:09 -0500

    feature:Merge de la inversión y repetición del mensaje

--- Pareja_Castiblanco_Leal/reto3/Reto3.java
+++ Pareja_Castiblanco_Leal/reto3/Reto3.java
@@ -1,8 +1,15 @@
 package Pareja_Castiblanco_Leal.reto3;
 
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 public class Reto3 {
+    public static String repeatMessage(String message){
+        StringBuilder sb = new StringBuilder();
+        IntStream.range(0, 3).forEach(i -> sb.append(message).append(" "));
+
+        return sb.toString().trim();
+    }
     public static String invertMessage(String message){
         StringBuffer sb = new StringBuffer(message);
         return sb.reverse().toString();
@@ -10,7 +17,10 @@ public class Reto3 {
 
     public static void main(String[] args) {
         String message = "DOSW is awesome";
-        Function<String, String> fun = m -> invertMessage(m);
-        System.out.println(fun.apply(message));
+
+        Function<String,String> fun1 = m-> repeatMessage(m);
+        System.out.println(fun1.apply(message));
+        Function<String, String> fun2 = m -> invertMessage(m);
+        System.out.println(fun2.apply(message));
     }
 }
