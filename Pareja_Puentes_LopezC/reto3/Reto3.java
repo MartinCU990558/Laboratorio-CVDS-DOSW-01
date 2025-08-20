8b49a7479c572059c60abed5939273529b86658d
Nestor David <nestor.lopez-c@mail.escuelaing.edu.co>
2025-08-19 22:04:46 -0500

    Reto 3

--- Pareja_Puentes_LopezC/reto3/Reto3.java
+++ Pareja_Puentes_LopezC/reto3/Reto3.java
@@ -4,11 +4,26 @@ import java.util.stream.Stream;
 public class Reto3 {
 
     
-    public StringBuilder repeticion(String mensaje) {
+    public static StringBuilder repeticion(String mensaje) {
         StringBuilder resultado = new StringBuilder(); 
         Stream.generate(() -> mensaje)
             .limit(3).forEach(resultado::append);
     
     return resultado;
-}
+    }
+
+    public static StringBuffer reverso(String mensaje){
+        StringBuffer sb = new StringBuffer(mensaje);
+        return sb.reverse();
+    }
+    public static String combinacion(String mensaje) {
+        StringBuilder msj = repeticion(mensaje);      
+        StringBuffer ms = reverso(msj.toString());    
+        return ms.toString();                        
+    }
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+
 }
