dc959c08e22e6167dea66bfca188db4c95374f05
unknown <julian.castiblanco-r@labinfo.is.escuelaing.edu.co>
2025-08-20 18:55:53 -0500

    feature: reto4 merge

--- Pareja_Castiblanco_Leal/reto4/Reto4.java
+++ Pareja_Castiblanco_Leal/reto4/Reto4.java
@@ -1,31 +1,54 @@
 package Pareja_Castiblanco_Leal.reto4;
 
 import java.util.HashMap;
+import java.util.Hashtable; // Corregido
 import java.util.List;
 import java.util.Map;
 
 public class Reto4 {
+
     public static Map<String, Integer> mapaSinDuplicados(List<String> claves, List<Integer> valores) {
         Map<String, Integer> mapa = new HashMap<>();
-
         for (int i = 0; i < claves.size(); i++) {
             String clave = claves.get(i);
             Integer valor = valores.get(i);
-
             if (!mapa.containsKey(clave)) {
                 mapa.put(clave, valor);
             }
         }
+        return mapa;
+    }
 
+    public static Hashtable<String, Integer> mapaSinDuplicados2(List<String> claves, List<Integer> valores) {
+        Hashtable<String, Integer> mapa = new Hashtable<>();
+        for (int i = 0; i < claves.size(); i++) {
+            String clave = claves.get(i);
+            Integer valor = valores.get(i);
+            if (!mapa.containsKey(clave)) {
+                mapa.put(clave, valor);
+            }
+        }
         return mapa;
     }
 
     public static void imprimirMapa(Map<String, Integer> mapa) {
-        mapa.entrySet().stream().sorted(Map.Entry.comparingByKey()).map(entry -> entry.getKey() + " = " + entry.getValue()).forEach(System.out::println);
+        mapa.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(entry -> entry.getKey() + " = " + entry.getValue())
+                .forEach(System.out::println);
+    }
+
+    public static void imprimirMapa2(Hashtable<String, Integer> mapa) {
+        mapa.entrySet()
+                .stream()
+                .sorted((e1, e2) -> e1.getKey().compareTo(e2.getKey()))
+                .map(entry -> entry.getKey() + " = " + entry.getValue())
+                .forEach(System.out::println);
     }
 
     public static void main(String[] args) {
-        List<String> claves = List.of("DOSW", "AYSR", "PTIA", "AYSR", "COMP");
+        List<String> claves = List.of("dosw", "aysr", "ptia", "aysr", "comp");
         List<Integer> valores = List.of(1, 3, 5, 7, 9);
 
         if (claves.size() != valores.size()) {
@@ -33,6 +56,11 @@ public class Reto4 {
         } else {
             Map<String, Integer> mapa = mapaSinDuplicados(claves, valores);
             imprimirMapa(mapa);
+
+            System.out.println("-----------------------------------------------------------");
+
+            Hashtable<String, Integer> mapa2 = mapaSinDuplicados2(claves, valores);
+            imprimirMapa2(mapa2);
         }
     }
 }
