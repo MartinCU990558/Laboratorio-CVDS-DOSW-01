faa68d546584c50e7cb6e8185c9734e2b4968fb9
Julián Castiblanco <julian.castiblanco-r@mail.escuelaing.edu.co>
2025-08-19 21:39:11 -0500

    feature: String buffer invertido

--- Pareja_Castiblanco_Leal/reto3/Reto3.java
+++ Pareja_Castiblanco_Leal/reto3/Reto3.java
@@ -1,18 +1,16 @@
 package Pareja_Castiblanco_Leal.reto3;
 
 import java.util.function.Function;
-import java.util.stream.IntStream;
 
 public class Reto3 {
-    public static String repeatMessage(String message){
-        StringBuilder sb = new StringBuilder();
-        IntStream.range(0, 3).forEach(i -> sb.append(message).append(" "));
-
-        return sb.toString().trim();
+    public static String invertMessage(String message){
+        StringBuffer sb = new StringBuffer(message);
+        return sb.reverse().toString();
     }
+
     public static void main(String[] args) {
         String message = "DOSW is awesome";
-        Function<String, String> fun = m -> repeatMessage(m);
+        Function<String, String> fun = m -> invertMessage(m);
         System.out.println(fun.apply(message));
     }
 }
