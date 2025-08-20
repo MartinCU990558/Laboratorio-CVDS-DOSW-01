35d2b56953b7f30c5b2c24e9b41adae93729cad9
sebasPuentes <seb123.juli@gmail.com>
2025-08-19 21:58:01 -0500

    Arreglo del Merge

--- Pareja_Puentes_LopezC/reto3/Reto3.java
+++ Pareja_Puentes_LopezC/reto3/Reto3.java
@@ -4,11 +4,21 @@ import java.util.stream.Stream;
 public class Reto3 {
 
     
-    public StringBuilder repeticion(String mensaje) {
+    public static StringBuilder repeticion(String mensaje) {
         StringBuilder resultado = new StringBuilder(); 
         Stream.generate(() -> mensaje)
             .limit(3).forEach(resultado::append);
     
     return resultado;
-}
+    }
+
+    public static StringBuffer reverso(String mensaje){
+        StringBuffer sb = new StringBuffer(mensaje);
+        return sb.reverse();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+
 }
