27965131d0b5552c72723975ea4c317889f4470a
Santiago <santiago.carmona-p@mail.escuelaing.edu.co>
2025-08-19 22:12:47 -0500

    Se añadió el código de CarreraParalela

--- Pareja_Carmona_Patiño/reto2/CarreraParalela.java
+++ Pareja_Carmona_Patiño/reto2/CarreraParalela.java
@@ -1,5 +1,15 @@
 package reto2;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CarreraParalela {
-    
+    private static List<Integer> numeros = Arrays.asList(5, 12, 3, 8, 15, 1);
+
+    public static int encontrarNumMayor() {
+        return numeros.stream()
+                .max((a, b) -> a.compareTo(b))
+                .get();
+    }
 }
+
