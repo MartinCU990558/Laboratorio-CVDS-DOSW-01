c16ada9fb0ee0d56a7c0ecdb60de1e15504a2479
unknown <juan.lcruz@labinfo.is.escuelaing.edu.co>
2025-08-20 18:20:48 -0500

    feature:Creacion del Map sin duplicados

---
+++ Pareja_Castiblanco_Leal/reto4/Reto4.java
@@ -0,0 +1,38 @@
+package Pareja_Castiblanco_Leal.reto4;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Reto4 {
+    public static Map<String, Integer> mapaSinDuplicados(List<String> claves, List<Integer> valores) {
+        Map<String, Integer> mapa = new HashMap<>();
+
+        for (int i = 0; i < claves.size(); i++) {
+            String clave = claves.get(i);
+            Integer valor = valores.get(i);
+
+            if (!mapa.containsKey(clave)) {
+                mapa.put(clave, valor);
+            }
+        }
+
+        return mapa;
+    }
+
+    public static void imprimirMapa(Map<String, Integer> mapa) {
+        mapa.entrySet().stream().sorted(Map.Entry.comparingByKey()).map(entry -> entry.getKey() + " = " + entry.getValue()).forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        List<String> claves = List.of("DOSW", "AYSR", "PTIA", "AYSR", "COMP");
+        List<Integer> valores = List.of(1, 3, 5, 7, 9);
+
+        if (claves.size() != valores.size()) {
+            System.out.println("La cantidad de claves no coincide con la cantidad de valores");
+        } else {
+            Map<String, Integer> mapa = mapaSinDuplicados(claves, valores);
+            imprimirMapa(mapa);
+        }
+    }
+}
