36deeead3c8e47acfb92572f23a54258703fc4a1
Sergio Idarraga <chechotorres498@gmail.com>
2025-08-20 14:35:08 -0500

    nuevo print

--- Reto2/CarreraParalela.java
+++ Reto2/CarreraParalela.java
@@ -18,5 +18,7 @@ public class CarreraParalela {
         int minimo = calcularMinimo(numeros);
         int conteo = numeros.size();
         return new ResumenLista(maximo, minimo, conteo);
+
+        system.out.println("El máximo es: " + maximo);
     }
 }
