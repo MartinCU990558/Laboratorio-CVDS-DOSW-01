b0a5a418bb96839c4e56910c1b5e038ab43cbc53
Nestor David <nestor.lopez-c@mail.escuelaing.edu.co>
2025-08-21 17:45:52 -0500

    carril 1

--- Pareja_Puentes_LopezC/reto2/CarreraParalela.java
+++ Pareja_Puentes_LopezC/reto2/CarreraParalela.java
@@ -1,8 +1,14 @@
 package reto2;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 public class CarreraParalela {
-    
-    public static void main(String[] args) {
+    public static final java.util.function.Function<List<Integer>, Optional<Integer>> ENCONTRAR_MAXIMO = 
+        lista -> lista.stream()
+                     .max(Integer::compareTo);
+
+        public static void main(String[] args) {
         System.out.println("");
     }
 }
