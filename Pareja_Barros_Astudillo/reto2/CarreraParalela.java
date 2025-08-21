3f751f718447c89997b59feef34abd04fd1afc06
Sebastian Barros <sebastian.barros-b@mail.escuelaing.edu.co>
2025-08-21 01:16:54 -0500

    Conflicto resuelto en Reto #2: función unificada

--- Pareja_Barros_Astudillo/reto2/CarreraParalela.java
+++ Pareja_Barros_Astudillo/reto2/CarreraParalela.java
@@ -3,16 +3,31 @@ package Pareja_Barros_Astudillo.reto2;
 import java.util.*;
 
 public class CarreraParalela {
-<<<<<<< HEAD
-    public static int maximo(List<Integer> numeros) {
-        return numeros.stream().max(Integer::compare).orElseThrow();
-    }
-}
-=======
-    public static String minimoYCantidad(List<Integer> numeros) {
+
+    public static String resultadoLista(List<Integer> numeros) {
+        int max = numeros.stream().max(Integer::compare).orElseThrow();
         int min = numeros.stream().min(Integer::compare).orElseThrow();
         int cantidad = numeros.size();
-        return "Minimo: " + min + ", Cantidad: " + cantidad;
+
+        // Nueva lógica: múltiplo2 y divisor2
+        List<Integer> multiplos2 = new ArrayList<>();
+        List<Integer> divisores2 = new ArrayList<>();
+
+        for (int num : numeros) {
+            if (num % 2 == 0) {
+                multiplos2.add(num);
+            }
+            if (2 % num == 0) {
+                divisores2.add(num);
+            }
+        }
+
+        return "Maximo: " + max +
+               ", Minimo: " + min +
+               ", Cantidad: " + cantidad +
+               ", Multiplos de 2: " + multiplos2 +
+               ", Divisores de 2: " + divisores2;
     }
 }
->>>>>>> feature/reto_carril_dos_BarrosSebastian_AstudilloCarlos_2025-2
+
+
