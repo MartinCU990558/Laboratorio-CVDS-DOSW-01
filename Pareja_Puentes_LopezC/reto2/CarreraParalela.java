c6df2ff5009d0256f39fa2398d3f16600b425304
Nestor David <nestor.lopez-c@mail.escuelaing.edu.co>
2025-08-21 18:45:34 -0500

    carril 1

--- Pareja_Puentes_LopezC/reto2/CarreraParalela.java
+++ Pareja_Puentes_LopezC/reto2/CarreraParalela.java
@@ -1,22 +1,21 @@
 package reto2;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import javax.print.attribute.standard.NumberOfDocuments;
 
 public class CarreraParalela {
-    
+
     static int maximoNumero(List<Integer> numeros){
         return numeros.stream().max(Integer::compare).get();
+    }
+
+    static int cantidadNumeros(List<Integer> numeros) {
+        return (int) numeros.stream().count();
     }
     
     static int numeroMinimoCantidad(List<Integer> numeros){
         return numeros.stream().min(Integer::compare).get();
     }
 
-    static int cantidadNumeros(List<Integer> numeros) {
-        return (int) numeros.stream().count();
-    }
+
     static Resultados combinacion(List<Integer> numeros) {
         int maximo = maximoNumero(numeros);
         int minimo = numeroMinimoCantidad(numeros);
