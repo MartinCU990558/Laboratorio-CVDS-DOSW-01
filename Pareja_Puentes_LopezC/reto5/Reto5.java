7860ff3fedff4778188ea1fc39274b82affa2b4a
sebasPuentes <seb123.juli@gmail.com>
2025-08-20 23:20:25 -0500

    Reto6 Finalizado

--- Pareja_Puentes_LopezC/reto5/Reto5.java
+++ Pareja_Puentes_LopezC/reto5/Reto5.java
@@ -9,21 +9,45 @@ public class Reto5{
         return numeros.stream().filter(x -> x % 3 != 0)
                 .collect(Collectors.toCollection(HashSet::new));
     }
-    public static Set<Integer> ordenNatural(TreeSet<Integer> arbol) {
+
+    public static TreeSet<Integer> ordenNatural(TreeSet<Integer> arbol) {
         return arbol.stream()
             .filter(num -> num % 5 != 0)
             .collect(Collectors.toCollection(TreeSet::new));
     }
 
+    static TreeSet<Integer> unificacion(HashSet<Integer> hash, TreeSet<Integer> tree){
+        TreeSet<Integer> numerosUnidos = Stream.concat(hash.stream(), tree.stream())
+            .collect(Collectors.toCollection(TreeSet::new));
+        return numerosUnidos;
+
+    }
+
     public static void main(String[] args) {
 
-        HashSet<Integer> numeros = new HashSet<>(Arrays.asList(4, 9, 15, 7, 18, 21, 10, 5));
-        
-        HashSet<Integer> ceroNumeros3 = ceroTres(numeros);
+        Random random = new Random();
+        TreeSet<Integer> numerosTree = new TreeSet<>();
+        HashSet<Integer> numerosHash = new HashSet<>();
 
-        System.out.println(ceroNumeros3);
+        for(int i = 0; i < 10; i++){
+            int num = random.nextInt(100);
+            numerosTree.add(num);
+        }
 
-        
+        for(int i = 0; i < 10; i++){
+            int num = random.nextInt(100);
+            numerosHash.add(num);
+        }
+
+        TreeSet<Integer> numeros = ordenNatural(numerosTree);
+        HashSet<Integer> numeros1 = ceroTres(numerosHash);
+
+        System.out.println(numeros);
+        System.err.println(numeros1);
+
+        TreeSet<Integer> numerosUnidos = unificacion(numeros1, numeros);
+        numerosUnidos.forEach(n -> System.out.println("Número en arena: " + n));
+ 
     }
 
 }
