f30f7631f8a5f3b4ec95b0b107d98bc451b87751
JulianCReal <julian.castiblanco-r@mail.escuelaing.edu.co>
2025-08-20 23:14:46 -0500

    feature: arreglo del merge

--- Pareja_Castiblanco_Leal/reto5/Reto5.java
+++ Pareja_Castiblanco_Leal/reto5/Reto5.java
@@ -1,12 +1,13 @@
-package Pareja_Castiblanco_Leal.reto5;
+package reto5;
 
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.TreeSet;
 
 public class Reto5 {
-    public static Set<Integer> generarGuerreros(){
+    public static Set<Integer> generarGuerrerosHash(){
         Set<Integer> guerreros = new HashSet<>();
         Random random = new Random();
         for(int i = 0; i < random.nextInt(1,20); i++){
@@ -15,8 +16,32 @@ public class Reto5 {
 
         return guerreros.stream().filter(n -> n % 3 == 0).collect(Collectors.toSet());
     }
+    public static Set<Integer> generarGuerrerosTree(){
+        Set<Integer> guerreros = new TreeSet<>();
+        Random random = new Random();
+        for(int i = 0; i < random.nextInt(1,20); i++){
+            int numero = random.nextInt(100);
+            if(numero % 5 != 0){
+                guerreros.add(numero);
+            }
+        }
+        return guerreros;
+    }
     public static void main(String[] args) {
-        Set<Integer> guerreros = generarGuerreros();
-        System.out.println(guerreros);
+        Set<Integer> guerrerosHash = generarGuerrerosHash();
+        Set<Integer> guerrerosTree = generarGuerrerosTree();
+
+        Set<Integer> batalla = new TreeSet<>();
+        batalla.addAll(guerrerosHash);
+        batalla.addAll(guerrerosTree);
+
+        System.out.println("Guerreros Hash:");
+        guerrerosHash.forEach(n -> System.out.print(n + " "));
+
+        System.out.println("\n Guerreros Tree:");
+        guerrerosTree.forEach(n -> System.out.print(n + " "));
+
+        System.out.println("\n Batalla :");
+        batalla.forEach(n -> System.out.print(n + " "));
     }
 }
