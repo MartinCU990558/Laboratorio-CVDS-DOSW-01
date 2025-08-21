cc382965c5fd8ef43075212f6730718833483bdf
curso <raquel.selma-a@mail.escuelaing.edu.co>
2025-08-21 15:47:45 -0500

    Se añade la funcion lambda para calcular el maximo de una lista

---
+++ SelmaRaquel_SuarezAndres_2025-2/reto2/CarreraParalela.java
@@ -0,0 +1,20 @@
+package SelmaRquel_SuarezAndres_2025.reto2;
+
+public class CarreraParalela {
+
+    public static class Stats {
+
+        public static int maximo(List<Integer> datos) {
+            return datos.stream().mapToInt(Integer::intValue).max();
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
