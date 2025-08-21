49e0a62b2a6ca728845a578244ab4d930a15b84f
Julian <julianl116910@gmail.com>
2025-08-20 20:31:35 -0500

    Prueba Metodo Unificacion

--- Pareja_Lopez_Albarracin/reto5/Reto5.java
+++ Pareja_Lopez_Albarracin/reto5/Reto5.java
@@ -1,6 +1,7 @@
 package Pareja_Lopez_Albarracin.reto5;
 
 import java.util.HashSet;
+import java.util.TreeSet;
 import java.util.stream.*;
 public  class Reto5{
     public static void main(String args[]){
@@ -12,4 +13,12 @@ public  class Reto5{
 
     } 
 
+    public static void unification(HashSet<Integer> number1, TreeSet<Integer> number2) {
+        TreeSet<Integer> finalSet = Stream
+                .concat(number1.stream(), number2.stream()).sorted().distinct()
+                .collect(Collectors.toCollection(TreeSet::new));
+                finalSet.stream().forEach(System.out::println);
+    }
+
+    
 }
