f895ca93c4aff7ad5a9b5babd44fc2f8232b8887
Juan Andres <Juan.sfonesca@mail.escuelaing.edu.co>
2025-08-21 16:53:26 -0500

    Implementacion metodo string builder

---
+++ SelmaRaquel_SuarezAndres_2025-2/reto3/reto3.java
@@ -0,0 +1,16 @@
+package SelmaRquel_SuarezAndres_2025.reto3;
+import java.util.stream.IntStream;
+
+public class reto3 {
+
+
+    public static String repetir3_Builder(String msg) {
+        StringBuilder sb = new StringBuilder();
+        IntStream.range(0, 3).forEach(i -> {
+            if (i > 0) sb.append(' ');
+            sb.append(msg);
+        });
+        return sb.toString();
+    }
+
+}
