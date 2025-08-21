34b9052ebe63c6d497788a624cc193a93ab93d68
Karol2905 <karol.estupinan-v@mail.escuelaing.edu.co>
2025-08-20 23:17:30 -0500

    metodo igual

--- Reto4/Reto4.java
+++ Reto4/Reto4.java
@@ -13,6 +13,20 @@ public class Reto4 {
         return mapa;
     }
 
+    public static Map<String, Integer> combinarMapas(Map<String, Integer> mapaHashMap, Map<String, Integer> mapaHashTable) {
+
+        Map<String, Integer> mapaFinal = new HashMap<>(mapaHashMap);
+
+        for (Map.Entry<String, Integer> entrada : mapaHashTable.entrySet()) {
+            mapaFinal.put(entrada.getKey(), entrada.getValue());
+        }
+
+        return mapaFinal;
+    }
+
+
+
+
     public static void main(String[] args) {
         List<Map.Entry<String, Integer>> entradasHash = Arrays.asList(
                 new AbstractMap.SimpleEntry<>("oro", 5),
@@ -23,5 +37,14 @@ public class Reto4 {
 
         Map<String, Integer> mapaHash = crearMapaHashMap(entradasHash);
         System.out.println(mapaHash);
+
+        Map<String, Integer> mapaTable = new Hashtable<>();
+        mapaTable.put("oro", 20);
+        mapaTable.put("esmeralda", 15);
+
+
+        Map<String, Integer> mapaFinal = combinarMapas(mapaHash, mapaTable);
+        System.out.println(mapaFinal);
     }
+
 }
