7781fe01397a9a0723a69d1a0477cfebd4486c1f
ALEJO21005 <sergio.idarraga-t@mail.escuelaing.edu.co>
2025-08-20 22:56:40 -0500

    hash table

--- Reto4/Reto4.java
+++ Reto4/Reto4.java
@@ -12,6 +12,13 @@ public class Reto4 {
         }
         return mapa;
     }
+    public static Map<String, Integer> crearMapaHashTable(List<Map.Entry<String, Integer>> entradas) {
+        Map<String, Integer> tabla = new Hashtable<>();
+        for (Map.Entry<String, Integer> entrada : entradas) {
+            tabla.putIfAbsent(entrada.getKey(), entrada.getValue()); // sincronizado, conserva el primero
+        }
+        return tabla;
+    }
 
     public static void main(String[] args) {
         List<Map.Entry<String, Integer>> entradasHash = Arrays.asList(
@@ -20,8 +27,17 @@ public class Reto4 {
                 new AbstractMap.SimpleEntry<>("oro", 7),
                 new AbstractMap.SimpleEntry<>("diamante", 10)
         );
+        List<Map.Entry<String, Integer>> entradasTable = Arrays.asList(
+                new AbstractMap.SimpleEntry<>("plata", 8),
+                new AbstractMap.SimpleEntry<>("rubí", 4),
+                new AbstractMap.SimpleEntry<>("oro", 12),
+                new AbstractMap.SimpleEntry<>("esmeralda", 6)
+        );
 
         Map<String, Integer> mapaHash = crearMapaHashMap(entradasHash);
         System.out.println(mapaHash);
+
+        Map<String, Integer> mapaTable = crearMapaHashTable(entradasTable);
+        System.out.println(mapaTable);
     }
 }
