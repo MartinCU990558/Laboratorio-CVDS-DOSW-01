98b06b75ca5b8ad48c9b6bfccf4fd9f2c1bf56a5
curso <raquel.selma-a@mail.escuelaing.edu.co>
2025-08-21 17:34:02 -0500

    Implementacion metodo que recibe una lista de pares clave valor usando HashTable

--- SelmaRaquel_SuarezAndres_2025-2/reto4/reto4.java
+++ SelmaRaquel_SuarezAndres_2025-2/reto4/reto4.java
@@ -1,17 +1,24 @@
 package SelmaRquel_SuarezAndres_2025.reto4;
-
+import java.util.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class reto4 {
 
-    // Crea HashMap ignorando duplicados (mantener primer valor)
     public static Map<String, Integer> mapFromPairsHashMap(List<Map.Entry<String, Integer>> pares) {
         Map<String, Integer> m = new HashMap<>();
         for (Map.Entry<String, Integer> e : pares) {
             m.putIfAbsent(e.getKey(), e.getValue());
         }
         return m;
+    }
+
+    public static Hashtable<String,Integer> mapFromPairsHashtable(List<Map.Entry<String,Integer>> pares) {
+        Hashtable<String,Integer> t = new Hashtable<>();
+        for (Map.Entry<String,Integer> e : pares) {
+            t.putIfAbsent(e.getKey(), e.getValue());
+        }
+        return t;
     }
 }
