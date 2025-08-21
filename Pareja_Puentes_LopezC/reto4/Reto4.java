682956f4090463af7a6090eac596950ed0713528
Nestor David <nestor.lopez-c@mail.escuelaing.edu.co>
2025-08-21 01:17:00 -0500

    punto 4

--- Pareja_Puentes_LopezC/reto4/Reto4.java
+++ Pareja_Puentes_LopezC/reto4/Reto4.java
@@ -1,13 +1,41 @@
 package reto4;
-import java.util.HashMap;
-import  java.util.Hashtable;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-public class Reto4 {
+import java.util.*;
+import java.util.stream.*;
+
+public class Reto4{
+
     static Hashtable<String, Integer> table(List<Map.Entry<String, Integer>> par) {
         return par.stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
             (y, x) -> y,Hashtable::new));
     }
+    static HashMap<String, Integer> guardarEnHashMap(List<Map.Entry<String, Integer>> pares){
+        return pares.stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+            (y, x) -> y,HashMap::new));
+    }
+    static HashMap<String,Integer> combinado (Hashtable<String,Integer> hash,HashMap<String,Integer> hash2){
+        return Stream.concat(hash.entrySet().stream(),hash2.entrySet().stream()).collect(Collectors.toMap(
+            Map.Entry::getKey,
+            Map.Entry::getValue,
+            (valor1, valor2) -> valor1, 
+            HashMap::new
+        ));
+    }
+     static HashMap<String, Integer> ordenarPorClaveAscendente(Map<String, Integer> mapa) {
+        return mapa.entrySet().stream().sorted(Map.Entry.comparingByKey()).collect(Collectors.toMap(
+                Map.Entry::getKey,
+                Map.Entry::getValue,
+                (existente, nuevo) -> existente,
+                LinkedHashMap::new
+            ));
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
+
+
+
+
+
 
