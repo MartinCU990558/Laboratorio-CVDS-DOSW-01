6028d0c62c690a4f2c82298204ac356a6cc2c044
ALEJO21005 <sergio.idarraga-t@mail.escuelaing.edu.co>
2025-08-20 23:28:05 -0500

    metodo combinado estudiante 2

--- Reto4/Reto4.java
+++ Reto4/Reto4.java
@@ -5,13 +5,6 @@ import java.util.*;
 
 public class Reto4 {
 
-    public static Map<String, Integer> crearMapaHashMap(List<Map.Entry<String, Integer>> entradas) {
-        Map<String, Integer> mapa = new HashMap<>();
-        for (Map.Entry<String, Integer> entrada : entradas) {
-            mapa.putIfAbsent(entrada.getKey(), entrada.getValue()); 
-        }
-        return mapa;
-    }
     public static Map<String, Integer> crearMapaHashTable(List<Map.Entry<String, Integer>> entradas) {
         Map<String, Integer> tabla = new Hashtable<>();
         for (Map.Entry<String, Integer> entrada : entradas) {
@@ -19,14 +12,27 @@ public class Reto4 {
         }
         return tabla;
     }
+    public static Map<String, Integer> combinarMapas(Map<String, Integer> mapaHashMap, Map<String, Integer> mapaHashTable) {
+
+        Map<String, Integer> mapaFinal = new HashMap<>(mapaHashMap);
+
+        for (Map.Entry<String, Integer> entrada : mapaHashTable.entrySet()) {
+            mapaFinal.put(entrada.getKey(), entrada.getValue());
+        }
+
+        return mapaFinal;
+    }
 
     public static void main(String[] args) {
+        // Datos de ejemplo para HashMap (lo simula estudiante 1)
         List<Map.Entry<String, Integer>> entradasHash = Arrays.asList(
                 new AbstractMap.SimpleEntry<>("oro", 5),
                 new AbstractMap.SimpleEntry<>("plata", 3),
                 new AbstractMap.SimpleEntry<>("oro", 7),
                 new AbstractMap.SimpleEntry<>("diamante", 10)
         );
+
+        // Datos de ejemplo para Hashtable (lo hace este estudiante)
         List<Map.Entry<String, Integer>> entradasTable = Arrays.asList(
                 new AbstractMap.SimpleEntry<>("plata", 8),
                 new AbstractMap.SimpleEntry<>("rubí", 4),
@@ -34,10 +40,16 @@ public class Reto4 {
                 new AbstractMap.SimpleEntry<>("esmeralda", 6)
         );
 
-        Map<String, Integer> mapaHash = crearMapaHashMap(entradasHash);
-        System.out.println(mapaHash);
+        Map<String, Integer> mapaHash = new HashMap<>();
+        for (Map.Entry<String, Integer> entrada : entradasHash) {
+            mapaHash.putIfAbsent(entrada.getKey(), entrada.getValue());
+        }
 
         Map<String, Integer> mapaTable = crearMapaHashTable(entradasTable);
-        System.out.println(mapaTable);
+
+        Map<String, Integer> mapaFinal = combinarMapas(mapaHash, mapaTable);
+        System.out.println("Combinado: " + mapaFinal);
     }
+
+
 }
