d2ce8715c8b83cb41e2221f86d9315e5a63a67af
Juan Andres <Juan.sfonesca@mail.escuelaing.edu.co>
2025-08-21 15:59:23 -0500

    Implementacion expresion lamba que permite calcular el numero mas pequeño de una lista

--- SelmaRaquel_SuarezAndres_2025-2/reto2/CarreraParalela.java
+++ SelmaRaquel_SuarezAndres_2025-2/reto2/CarreraParalela.java
@@ -1,12 +1,26 @@
 package SelmaRquel_SuarezAndres_2025.reto2;
+import java.util.*;
+
+
+
+
 
 public class CarreraParalela {
 
     public static class Stats {
 
-        public static int maximo(List<Integer> datos) {
-            return datos.stream().mapToInt(Integer::intValue).max();
+        public static Integer maximo(List<Integer> datos) {
+            return datos.stream().mapToInt(Integer::intValue).max().orElse(0);
         }
+
+
+        public static Map.Entry<Integer,Integer> minimoYConteo(List<Integer> datos) {
+            int min = datos.stream().mapToInt(Integer::intValue).min()
+                    .orElseThrow(() -> new IllegalArgumentException("Lista vacía"));
+            int count = datos.size();
+            return Map.entry(min, count);
+        }
+
 
 
 
