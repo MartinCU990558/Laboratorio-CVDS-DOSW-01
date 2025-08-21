b18c4ca66ed8ca0c60038db3c1367d03fdcd7c18
JOSE BARERRO <sebastian.albarracin@mail.escuelaing.edu.co>
2025-08-20 20:20:18 -0500

    Main

--- Pareja_Lopez_Albarracin/reto5/Reto5.java
+++ Pareja_Lopez_Albarracin/reto5/Reto5.java
@@ -3,7 +3,9 @@ import java.util.TreeSet;
 import java.util.HashSet;
 import java.util.stream.*;
 public class Reto5{
-    
+    public static void main(String[] args) {
+        
+    }
 
     public static TreeSet<Integer> deleteMultiplesFive(TreeSet<Integer> numbers){
         return numbers.stream().filter(x-> x % 5!=0).collect(Collectors.toCollection(TreeSet::new));
