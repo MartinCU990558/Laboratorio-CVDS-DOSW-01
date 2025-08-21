348aca1f80811c3c99a5740a7fa162ed45bc3e2f
SantiagoSu15 <sasupu08@gmail.com>
2025-08-20 19:44:05 -0500

    "Primer choque reto 2"

--- Pareja_Sanchez_Suarez/reto2/CarreraParalela.java
+++ Pareja_Sanchez_Suarez/reto2/CarreraParalela.java
@@ -1,4 +1,32 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Optional;
+
 public class CarreraParalela {
+    private int numeroGrande(ArrayList<Integer> arry){
+        int a = 0;
+        if(arry.size() != 0){
+             a = arry.stream().max(Integer::compare).get();
+        }
+        return  a;
+    }
+
+
+
+
+    private int[] ambosJuntos(ArrayList<Integer> arry){
+        int[] numeros = new int[3];
+        numeros[0] = numeroGrande(arry);
+        numeros[1] = numeroPequeño(arry)[0];
+        numeros[2] = numeroPequeño(arry)[1];
+        return  numeros;
+    }
+
+
+    
+
     public static void main(String[] args) {
+
+
     }
 }
