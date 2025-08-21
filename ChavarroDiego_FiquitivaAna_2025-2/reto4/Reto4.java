8fddb697c20d4f722d69a18c7e9d7e13b55c1b7e
Ana Fiquitiva <ana.fiquitiva-p@mail.escuelaing.edu.co>
2025-08-21 18:07:18 -0500

    Estructura base del Reto4.java

---
+++ ChavarroDiego_FiquitivaAna_2025-2/reto4/Reto4.java
@@ -0,0 +1,34 @@
+
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Reto4 {
+    public static void main(String[] args) {
+        // Aquí van las pruebas al final
+    }
+
+    
+    public static Map<String,Integer> crearMapaHashMap(List<AbstractMap.SimpleEntry<String,Integer>> lista) {
+        
+        return new HashMap<>();
+    }
+
+    
+    public static Map<String,Integer> crearMapaHashTable(List<AbstractMap.SimpleEntry<String,Integer>> lista) {
+        
+        return new Hashtable<>();
+    }
+
+   
+    public static Map<String,Integer> combinarMapas(Map<String,Integer> hashMap, Map<String,Integer> hashTable) {
+        
+        return new HashMap<>();
+    }
+
+  
+    public static void imprimirMapa(Map<String,Integer> mapa) {
+        
+    }
+}
+
