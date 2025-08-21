7596b399cdbda4a9bd2df1122973dd061bde6e58
sebasPuentes <seb123.juli@gmail.com>
2025-08-21 00:44:57 -0500

    Funcion combinada

--- Pareja_Puentes_LopezC/reto4/Reto4.java
+++ Pareja_Puentes_LopezC/reto4/Reto4.java
@@ -1,6 +1,7 @@
 package reto4;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Reto4{
 
@@ -12,6 +13,12 @@ public class Reto4{
         return pares.stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
             (y, x) -> y,HashMap::new));
     }
+
+    static Map<String, Integer> combinado (HashMap<String, Integer> map, Hashtable<String, Integer> table) {
+
+    return Stream.concat(map.entrySet().stream(), table.entrySet().stream())
+        .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue(),(v1, v2) -> v2,HashMap::new));
+}
 
     public static void main(String[] args) {
         
