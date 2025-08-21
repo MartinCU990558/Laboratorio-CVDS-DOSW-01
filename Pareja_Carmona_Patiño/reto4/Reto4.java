6157307c786b5da5c1dec5a5be52db607de005b6
daniel-pm19 <daniel.patino-m@mail.escuelaing.edu.co>
2025-08-21 00:31:46 -0500

    Metodo lista de pares a hashmap completado

--- Pareja_Carmona_Patiño/reto4/Reto4.java
+++ Pareja_Carmona_Patiño/reto4/Reto4.java
@@ -1,5 +1,20 @@
 package reto4;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Reto4 {
+
+    private HashMap<String, Integer> mapaParteRapida = new HashMap<>();
+
+
+    public HashMap<String, Integer> almacenarMitadMapa(List<Map.Entry<String, Integer>> listaDePares) {         
+        listaDePares.stream()
+                .filter(par -> !mapaParteRapida.containsKey(par.getKey()))
+                .forEach(par -> mapaParteRapida.put(par.getKey(), par.getValue()));
+
+        return mapaParteRapida;
+    }
 
 }
