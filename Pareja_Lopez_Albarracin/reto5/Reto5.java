fdf73abf3c02ba7a490866182bbc92bb48102778
JOSE BARERRO <sebastian.albarracin@mail.escuelaing.edu.co>
2025-08-20 19:50:11 -0500

    Se realizo el metodo para eliminar los multiplos de 5

---
+++ Pareja_Lopez_Albarracin/reto5/Reto5.java
@@ -0,0 +1,12 @@
+package Pareja_Lopez_Albarracin.reto5;
+import java.util.TreeSet;
+import java.util.stream.*;
+public class Reto5{
+    public static void main(String args[]){
+
+    }
+
+    public static TreeSet<Integer> deleteMultiplesFive(TreeSet<Integer> numbers){
+        return numbers.stream.filter(x-> x % 5!=0).collect(Collectors.toCollecton(TreeSet::new));
+    }
+}
