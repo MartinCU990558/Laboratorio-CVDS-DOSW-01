5a6c6fa77af5d3408039a509f86e46f90c72c948
sebasPuentes <seb123.juli@gmail.com>
2025-08-21 01:11:09 -0500

    Mejora Mayusculas!

--- Pareja_Puentes_LopezC/reto4/Reto4.java
+++ Pareja_Puentes_LopezC/reto4/Reto4.java
@@ -1,17 +1,33 @@
 package reto4;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Reto4{
 
+    static Hashtable<String, Integer> table(List<Map.Entry<String, Integer>> par) {
+        return par.stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+            (y, x) -> y,Hashtable::new));
+    }
     static HashMap<String, Integer> guardarEnHashMap(List<Map.Entry<String, Integer>> pares){
         return pares.stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
             (y, x) -> y,HashMap::new));
     }
 
+    static Map<String, Integer> combinado(HashMap<String, Integer> map, Hashtable<String, Integer> table) {
+        return Stream.concat(map.entrySet().stream(), table.entrySet().stream())
+            .collect(Collectors.toMap(
+                e -> e.getKey().toUpperCase(),Map.Entry::getValue,(v1, v2) -> v2,TreeMap::new));
+    }
+
+
     public static void main(String[] args) {
-        
+
     }
     
 }
+
+
+
+
 
