4e7d1c142f1a4be5da6efcc360cb668c59e3ceec
sebasPuentes <seb123.juli@gmail.com>
2025-08-21 18:20:04 -0500

    Funcion Combinacion

--- Pareja_Puentes_LopezC/reto2/CarreraParalela.java
+++ Pareja_Puentes_LopezC/reto2/CarreraParalela.java
@@ -1,21 +1,25 @@
 package reto2;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import javax.print.attribute.standard.NumberOfDocuments;
 
 public class CarreraParalela {
-    
+
     static int maximoNumero(List<Integer> numeros){
         return numeros.stream().max(Integer::compare).get();
+    }
+
+    static int cantidadNumeros(List<Integer> numeros) {
+        return (int) numeros.stream().count();
     }
     
     static int numeroMinimoCantidad(List<Integer> numeros){
         return numeros.stream().min(Integer::compare).get();
     }
 
-    static int cantidadNumeros(List<Integer> numeros) {
-        return (int) numeros.stream().count();
+    static Resultados combinacion(List<Integer> numeros){
+        Resultados resultados = new Resultados(maximoNumero(numeros),
+                                               numeroMinimoCantidad(numeros),
+                                               cantidadNumeros(numeros));
+        return resultados;
     }
 
     public static void main(String[] args) {
