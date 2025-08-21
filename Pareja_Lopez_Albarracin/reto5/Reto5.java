d007782d77adce1e14058e2184ef439bdce90490
JOSE BARERRO <sebastian.albarracin@mail.escuelaing.edu.co>
2025-08-20 20:14:16 -0500

    Unificacion Metodo

--- Pareja_Lopez_Albarracin/reto5/Reto5.java
+++ Pareja_Lopez_Albarracin/reto5/Reto5.java
@@ -1,12 +1,15 @@
 package Pareja_Lopez_Albarracin.reto5;
 import java.util.TreeSet;
+import java.util.HashSet;
 import java.util.stream.*;
 public class Reto5{
-    public static void main(String args[]){
-
-    }
+    
 
     public static TreeSet<Integer> deleteMultiplesFive(TreeSet<Integer> numbers){
-        return numbers.stream.filter(x-> x % 5!=0).collect(Collectors.toCollecton(TreeSet::new));
+        return numbers.stream().filter(x-> x % 5!=0).collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    public static void unification(HashSet<Integer> numbers1,TreeSet<Integer> numbers2){
+
     }
 }
