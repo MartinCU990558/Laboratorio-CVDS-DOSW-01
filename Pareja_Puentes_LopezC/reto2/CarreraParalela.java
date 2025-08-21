d737ea1545c28a8f6226b2056b4a9eddffec5ca4
Nestor David <nestor.lopez-c@mail.escuelaing.edu.co>
2025-08-21 18:23:02 -0500

    choque 1 carril 1

--- Pareja_Puentes_LopezC/reto2/CarreraParalela.java
+++ Pareja_Puentes_LopezC/reto2/CarreraParalela.java
@@ -2,13 +2,28 @@ package reto2;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import javax.print.attribute.standard.NumberOfDocuments;
 
 public class CarreraParalela {
-    public static final java.util.function.Function<List<Integer>, Optional<Integer>> ENCONTRAR_MAXIMO = 
-        lista -> lista.stream()
-                     .max(Integer::compareTo);
+    
+    static int maximoNumero(List<Integer> numeros){
+        return numeros.stream().max(Integer::compare).get();
+    }
+    
+    static int numeroMinimoCantidad(List<Integer> numeros){
+        return numeros.stream().min(Integer::compare).get();
+    }
 
-        public static void main(String[] args) {
+    static int cantidadNumeros(List<Integer> numeros) {
+        return (int) numeros.stream().count();
+    }
+    static Resultados combinacion(List<Integer> numeros) {
+        int maximo = maximoNumero(numeros);
+        int minimo = numeroMinimoCantidad(numeros);
+        int cantidad = cantidadNumeros(numeros);
+        return new Resultados(maximo, minimo, cantidad);
+    }
+    public static void main(String[] args) {
         System.out.println("");
     }
 }
