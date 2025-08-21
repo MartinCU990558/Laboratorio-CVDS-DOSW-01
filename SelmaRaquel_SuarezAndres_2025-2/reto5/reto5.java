19b3e4e18ee17f43b2cfdaf0b127367ce1b7fb3e
curso <raquel.selma-a@mail.escuelaing.edu.co>
2025-08-21 18:30:42 -0500

    Creacion de metodo que almacena numeros aleatroios en orden ascendente y elimina los multiplos de 5

--- SelmaRaquel_SuarezAndres_2025-2/reto5/reto5.java
+++ SelmaRaquel_SuarezAndres_2025-2/reto5/reto5.java
@@ -10,4 +10,8 @@ public class reto5 {
     public static Set<Integer> hashSetSinMultiplosDe3(Collection<Integer> nums) {
         return nums.stream().filter(n -> n % 3 != 0).collect(Collectors.toCollection(HashSet::new));
     }
+
+    public static Set<Integer> treeSetSinMultiplosDe5(Collection<Integer> nums) {
+        return nums.stream().filter(n -> n % 5 != 0).collect(Collectors.toCollection(TreeSet::new));
+    }
 }
