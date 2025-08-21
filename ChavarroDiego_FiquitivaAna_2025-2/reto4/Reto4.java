111bd868af27895e3de09a3859eb8fa4821e3605
Ana Fiquitiva <diego.chavarro-c@mail.escuelaing.edu.co>
2025-08-21 18:50:59 -0500

    Crear Hashtable y combinar con claves ordenadas ascendentemente

--- ChavarroDiego_FiquitivaAna_2025-2/reto4/Reto4.java
+++ ChavarroDiego_FiquitivaAna_2025-2/reto4/Reto4.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class Reto4 {
     public static void main(String[] args) {
-        // Aquí van las pruebas al final
+        
     }
 
     
@@ -14,17 +14,28 @@ public class Reto4 {
         return new HashMap<>();
     }
 
-    
     public static Map<String,Integer> crearMapaHashTable(List<AbstractMap.SimpleEntry<String,Integer>> lista) {
-        
-        return new Hashtable<>();
+    Map<String,Integer> mapa = new Hashtable<>();
+    for (AbstractMap.SimpleEntry<String,Integer> par : lista) {
+        mapa.putIfAbsent(par.getKey(), par.getValue()); // sincronizado
     }
+    return mapa;
+}
 
-   
-    public static Map<String,Integer> combinarMapas(Map<String,Integer> hashMap, Map<String,Integer> hashTable) {
-        
-        return new HashMap<>();
-    }
+public static Map<String,Integer> combinarMapas(Map<String,Integer> hashMap, Map<String,Integer> hashTable) {
+    Map<String,Integer> combinado = new HashMap<>(hashMap);
+    combinado.putAll(hashTable);
+
+    return combinado.entrySet().stream()
+        .sorted(Map.Entry.comparingByKey())
+        .collect(Collectors.toMap(
+            Map.Entry::getKey,
+            Map.Entry::getValue,
+            (v1, v2) -> v2,
+            LinkedHashMap::new
+        ));
+}
+
 
   
     public static void imprimirMapa(Map<String,Integer> mapa) {
