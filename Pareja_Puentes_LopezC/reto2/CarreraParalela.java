80edba6da3b4fa020247ac92c5da777b349e0b22
sebasPuentes <seb123.juli@gmail.com>
2025-08-21 18:02:18 -0500

    Implementacion MINIMO Y CANTIDAD

--- Pareja_Puentes_LopezC/reto2/CarreraParalela.java
+++ Pareja_Puentes_LopezC/reto2/CarreraParalela.java
@@ -2,13 +2,23 @@ package reto2;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import javax.print.attribute.standard.NumberOfDocuments;
 
 public class CarreraParalela {
-    public static final java.util.function.Function<List<Integer>, Optional<Integer>> ENCONTRAR_MAXIMO = 
-        lista -> lista.stream()
-                     .max(Integer::compareTo);
+    
+    static int maximoNumero(List<Integer> numeros){
+        return numeros.stream().max(Integer::compare).get();
+    }
+    
+    static int numeroMinimoCantidad(List<Integer> numeros){
+        return numeros.stream().min(Integer::compare).get();
+    }
 
-        public static void main(String[] args) {
+    static int cantidadNumeros(List<Integer> numeros) {
+        return (int) numeros.stream().count();
+    }
+
+    public static void main(String[] args) {
         System.out.println("");
     }
 }
