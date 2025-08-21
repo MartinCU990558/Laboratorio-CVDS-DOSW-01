bfe66efb4fd695144163795606fa432223a50ab1
unknown <tulio.riano-s@labinfo.is.escuelaing.edu.co>
2025-08-21 10:55:02 -0500

    Se actualizo reto 6 prints y falta primera parte del cuestionario

--- Pareja_Nieto_Riano/reto6/Reto6.java
+++ Pareja_Nieto_Riano/reto6/Reto6.java
@@ -6,16 +6,16 @@ public class Reto6 {
     public static void fragmentoUno(String mensaje) {
         switch (mensaje) {
             case "SALUDAR":
-                System.out.println("Saludos, viajero del tiempo y del codigo!");
+                System.out.println("La máquina dice: ¡Saludos, viajero del tiempo y del codigo!");
                 break;
             case "DESPEDIR":
-                System.out.println("Que los bits te acompanen, hasta la proxima mision.");
+                System.out.println("La máquina dice: Que los bits te acompanen, hasta la proxima mision.");
                 break;
             case "CANTAR":
-                System.out.println("01010101");
+                System.out.println("La máquina canta: 01010101");
                 break;
             case "DANZAR":
-                System.out.println("Girando en modo fiesta.");
+                System.out.println("La máquina gira y emite chispas: Girando en modo fiesta.");
                 break;
         }
     }
@@ -23,16 +23,16 @@ public class Reto6 {
     public static void fragmentoDos(String mensaje) {
         switch (mensaje) {
             case "BROMEAR":
-                System.out.println("Por que la RAM rompio con la CPU? Porque necesitaba espacio...");
+                System.out.println("La máquina rie y dice: Por que la RAM rompio con la CPU? Porque necesitaba espacio...");
                 break;
             case "GRITAR":
-                System.out.println("!!!ALERTA DE STACK OVERFLOW!!!");
+                System.out.println("La máquina grita: !!!ALERTA DE STACK OVERFLOW!!!");
                 break;
             case "SUSURRAR":
-                System.out.println("Shhh... los bugs estan dormidos.");
+                System.out.println("La máquina susurra: Shhh... los bugs estan dormidos.");
                 break;
             case "ANALIZAR":
-                System.out.println("Analizando datos... resultado: Eres increible programando!");
+                System.out.println("La máquina procesa: Analizando datos... resultado: Eres increible programando!");
                 break;
         }
     }
@@ -40,28 +40,28 @@ public class Reto6 {
     public static void unificacionFragmentos(String mensaje) {
         switch (mensaje) {
             case "SALUDAR":
-                System.out.println("¡Saludos, viajero del tiempo y del código!");
+                System.out.println("La máquina dice: ¡Saludos, viajero del tiempo y del código!");
                 break;
             case "DESPEDIR":
-                System.out.println("Que los bits te acompañen, hasta la próxima misión.");
+                System.out.println("La máquina dice: Que los bits te acompañen, hasta la próxima misión.");
                 break;
             case "CANTAR":
-                System.out.println("01010101");
+                System.out.println("La máquina canta: 01010101");
                 break;
             case "DANZAR":
-                System.out.println("Girando en modo fiesta.");
+                System.out.println("La máquina gira y emite chispas: Girando en modo fiesta.");
                 break;
             case "BROMEAR":
-                System.out.println("¿Por qué la RAM rompió con la CPU? Porque necesitaba espacio...");
+                System.out.println("La máquina rie y dice... ¿Por qué la RAM rompió con la CPU? Porque necesitaba espacio...");
                 break;
             case "GRITAR":
-                System.out.println("¡¡¡ALERTA DE STACK OVERFLOW!!!");
+                System.out.println("La máquina grita...: ¡¡¡ALERTA DE STACK OVERFLOW!!!");
                 break;
             case "SUSURRAR":
-                System.out.println("Shhh... los bugs están dormidos.");
+                System.out.println("La máquina susurra... Shhh... los bugs están dormidos.");
                 break;
             case "ANALIZAR":
-                System.out.println("Analizando datos... resultado: ¡Eres increíble programando!");
+                System.out.println("La máquina procesa...Analizando datos... resultado: ¡Eres increíble programando!");
                 break;
             default:
                 System.out.println("NO COMMAND VALID");
