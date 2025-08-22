d5ae1fca3f99da7e520039ed4702637dd7c4b5e7
Julian <julianl116910@gmail.com>
2025-08-21 22:36:06 -0500

    Resultados

---
+++ Pareja_Lopez_Albarracin/reto2/Resultados.java
@@ -0,0 +1,34 @@
+package Pareja_Lopez_Albarracin.reto2;
+
+public class Resultados {
+    private int maximo;
+    private int minimo;
+    private int cantidad;
+
+    public Resultados(int maximo, int minimo, int cantidad) {
+        this.maximo = maximo;
+        this.minimo = minimo;
+        this.cantidad = cantidad;
+    }
+
+    public int getMaximo() {
+        return maximo;
+    }
+
+    public int getMinimo() {
+        return minimo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Resultados{" +
+                "maximo=" + maximo +
+                ", minimo=" + minimo +
+                ", cantidad=" + cantidad +
+               '}';
+   }
+}
