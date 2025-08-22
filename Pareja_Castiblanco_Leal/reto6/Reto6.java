c63e82562b5d43c22c5e380fa1851138e890d060
Juan Leal <lecrujuan@gmail.com>
2025-08-21 20:56:00 -0500

    feature:Actualización del reto por error en los mensajes

--- Pareja_Castiblanco_Leal/reto6/Reto6.java
+++ Pareja_Castiblanco_Leal/reto6/Reto6.java
@@ -7,7 +7,7 @@ public class Reto6 {
     private static Map<String, Runnable> comandos = new HashMap<>();
 
     static{
-        comandos.put("SALUDAR", ()-> System.out.println("La máquina ríe: ¿Por qué la RAM rompió con la CPU? Porque necesitaba espacio..."));
+        comandos.put("SALUDAR", ()-> System.out.println("La máquina dice: \"¡Saludos, viajero del tiempo y del código!\""));
         comandos.put("DESPEDIR", ()-> System.out.println("La máquina dice: \"Que los bits te acompañen, hasta la próxima misión.\""));
         comandos.put("CANTAR", ()-> System.out.println("La máquina canta: \"01010101\""));
         comandos.put("DANZAR", ()-> System.out.println("La máquina gira y emite chispas: \"Girando en modo fiesta.\""));
