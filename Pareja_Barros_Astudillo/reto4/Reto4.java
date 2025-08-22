58f471aa37991212f11ef5fcc114a9c5d5fa95db
carlos astudillo <carlos.astudillo-c@mail.escuelaing.edu.co>
2025-08-22 02:06:30 -0500

    tercer metodo unificado reto 4

--- Pareja_Barros_Astudillo/reto4/Reto4.java
+++ Pareja_Barros_Astudillo/reto4/Reto4.java
@@ -12,13 +12,33 @@ public class Reto4 {
         return mapa;
     }
 
-    // Método con Hashtable (segundo metodo pedido)
+    // Metodo con Hashtable (segundo metodo pedido)
     public static Map<String, Integer> crearHashTable(List<AbstractMap.SimpleEntry<String, Integer>> lista) {
         Map<String, Integer> tabla = new Hashtable<>();
         for (AbstractMap.SimpleEntry<String, Integer> par : lista) {
             tabla.putIfAbsent(par.getKey(), par.getValue());
         }
         return tabla;
+    }
+
+    // Metodo unificado por ambos
+    public static void combinarEImprimir(Map<String, Integer> hashMap, Map<String, Integer> hashTable) {
+        
+        Map<String, Integer> combinado = new HashMap<>(hashMap);
+        hashTable.forEach((k, v) -> combinado.put(k, v)); 
+
+        
+        Map<String, Integer> resultado = combinado.entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        e -> e.getKey().toUpperCase(), 
+                        Map.Entry::getValue,
+                        (v1, v2) -> v1, 
+                        TreeMap::new 
+                ));
+
+        // Paso 3: imprimir
+        resultado.forEach((k, v) -> System.out.println("Clave: " + k + " | Valor: " + v));
     }
 
 }
