f775add403d7b144252ee5609cd11412ffbdfd96
Julian <julianl116910@gmail.com>
2025-08-21 19:40:35 -0500

    HashMap Hecho

---
+++ Pareja_Lopez_Albarracin/reto4/Reto4.java
@@ -0,0 +1,16 @@
+package Pareja_Lopez_Albarracin.reto4;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Reto4 {
+
+    public static Map<String, Integer> createHashMap(List<Map.Entry<String, Integer>> list) {
+    Map<String, Integer> map = new HashMap<>();
+    for (Map.Entry<String, Integer> par : list) {
+        map.putIfAbsent(par.getKey(), par.getValue());
+    }
+    return map;
+}
+}
