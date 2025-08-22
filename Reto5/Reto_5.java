ffd99f74c9ff4e493ad96aed924d9b654c2d352b
Sergio Idarraga <chechotorres498@gmail.com>
2025-08-21 23:16:01 -0500

    Reto #5 terminado

--- Reto5/Reto_5.java
+++ Reto5/Reto_5.java
@@ -1,20 +1,30 @@
 package Idarraga_Estupinan.Reto5;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Reto_5 {
 
     public static HashSet<Integer> hash(List<Integer> numeros) {
-
-        HashSet<Integer> num_aleatorios = new HashSet<>(numeros);
-        return (HashSet<Integer>) num_aleatorios.stream().filter(n -> n % 3 != 0).collect(Collectors.toSet());
+        return numeros.stream()
+                .filter(n -> n % 3 != 0)
+                .collect(Collectors.toCollection(HashSet::new));
     }
+
     public static TreeSet<Integer> tree(List<Integer> numeros) {
-
-        TreeSet<Integer> num_aleatorios = new TreeSet<>(numeros);
-        return num_aleatorios.stream().filter(n -> n % 5 != 0).collect(Collectors.toSet());
+        return numeros.stream()
+                .filter(n -> n % 5 != 0)
+                .collect(Collectors.toCollection(TreeSet::new));
     }
-}    
+
+    public static void main(String[] args) {
+        List<Integer> numerosHash = Arrays.asList(4, 9, 15, 7, 18, 21, 10, 5);
+        List<Integer> numerosTree = Arrays.asList(12, 3, 25, 7, 10, 12, 18);
+
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        treeSet.addAll(hash(numerosHash));
+        treeSet.addAll(tree(numerosTree));
+
+        treeSet.forEach(n -> System.out.print("Número en arena: " + n + "\n"));
+    }
+}
