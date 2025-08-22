7ab2676cc851f226f6b8af79165cd9232238c229
Ana Fiquitiva <ana.fiquitiva-p@mail.escuelaing.edu.co>
2025-08-21 20:42:27 -0500

    Estructura inicial Reto 6

---
+++ ChavarroDiego_FiquitivaAna_2025-2/reto6/Reto6.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Reto6 {
+
+    private static Map<String, Runnable> comandos = new HashMap<>();
+
+    public static void ejecutarComando(String comando) {
+        
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
