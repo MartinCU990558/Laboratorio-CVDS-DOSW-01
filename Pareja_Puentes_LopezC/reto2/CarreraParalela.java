2fc74235f3be89aae8dfd7e13d01278e97856ca7
Nestor David <nestor.lopez-c@mail.escuelaing.edu.co>
2025-08-21 19:14:42 -0500

    Cambios carril 1

--- Pareja_Puentes_LopezC/reto2/CarreraParalela.java
+++ Pareja_Puentes_LopezC/reto2/CarreraParalela.java
@@ -1,6 +1,6 @@
 package reto2;
 import java.util.List;
-
+import reto2.Resultados;
 public class CarreraParalela {
 
     static int maximoNumero(List<Integer> numeros){
@@ -14,16 +14,18 @@ public class CarreraParalela {
     static int numeroMinimoCantidad(List<Integer> numeros){
         return numeros.stream().min(Integer::compare).get();
     }
-    static boolean multiplodedos(List<Integer> numeros){
-        int maximo = maximoNumero(numeros);
-        if (maximo % 2 == 0){
-            return true;
-        }
-        else{
-            return false;
-        }
+
+    static boolean numeroMayorEsDivisorDe2(int numero) {
+        return 2 % numero == 0 ? true : false;
     }
-    
+    static boolean esMultiploDeDos(int numero) {
+    return (numero % 2 == 0) ? true : false;
+    }
+    static boolean cantidadEsPar(List<Integer> numeros) {
+    return (numeros.size() % 2 == 0) ? true : false;
+    }
+
+
     static Resultados combinacion(List<Integer> numeros) {
         int maximo = maximoNumero(numeros);
         int minimo = numeroMinimoCantidad(numeros);
