46d1d784b83c0b39951eaa535744835ac59ab77f
daniel-pm19 <daniel.patino-m@mail.escuelaing.edu.co>
2025-08-21 21:11:19 -0500

    Tercer choque

--- Pareja_Carmona_Patiño/reto2/CarreraParalela.java
+++ Pareja_Carmona_Patiño/reto2/CarreraParalela.java
@@ -33,12 +33,13 @@ public class CarreraParalela {
     }
 
     public int[] obtenerDatosCarrera(){
-        int[] datos = new int[5];
+        int[] datos = new int[6];
         datos[0] = encontrarNumMayor(numeros);
         datos[1] = hallarMinimoYNumeroTotalDeDatos(numeros)[0];
         datos[2] = hallarMinimoYNumeroTotalDeDatos(numeros)[1];
         datos[3] = (encontrarNumMayor(numeros) % 2 == 0) ? 1 : 0;
         datos[4] = (2 % encontrarNumMayor(numeros) == 0) ? 1 : 0;
+        datos[5] = numeros.size() % 2 != 0 ? 1 : 0;
         return datos;
 
     }
