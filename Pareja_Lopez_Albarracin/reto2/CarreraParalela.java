a23a3ff82ab481e881a6575cd69cccc333d6782e
Sebastián Albarracín Silva <sebastian.albarracin@mail.escuelaing.edu.co>
2025-08-21 23:07:35 -0500

    ejercicio culminado

--- Pareja_Lopez_Albarracin/reto2/CarreraParalela.java
+++ Pareja_Lopez_Albarracin/reto2/CarreraParalela.java
@@ -1,23 +1,23 @@
+package Pareja_Lopez_Albarracin.reto2;
+
 import java.util.List;
 
 public class CarreraParalela {
+    public static int biggerNumber(List<Integer> numbers) {
+        return numbers.stream().max(Integer::compare).get();
+    }
 
     public static int minimumNumber(List<Integer> numbers) {
         return numbers.stream().min(Integer::compare).get();
-    }
-
-    public static int biggerNumber(List<Integer> numbers) {
-        return numbers.stream().max(Integer::compare).get();
     }
 
     public static Resultados combineResults(List<Integer> numbers){
         int max = biggerNumber(numbers);
         int min = minimumNumber(numbers);
         int n   = numbers.size();
-
         boolean esMultiplo = (max % 2 == 0) ? true : false;
-        return new Resultados(max, min, n, esMultiplo);
+        boolean esDivisor = (2 % max == 0) ? true : false;
+
+        return new Resultados(max, min, n, esMultiplo, esDivisor);
     }
-
 }
-
