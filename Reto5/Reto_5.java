1734e71b7ae8bc8356958d4f0e6c710f5bd456ca
Karol Estupiñan <karol.estupinan-v@mail.escuelaing.edu.co>
2025-08-21 22:46:04 -0500

    creacion metodo treeset que almacena numeros aleatorios en orden ascendete y elimina multiplos de 5

--- Reto5/Reto_5.java
+++ Reto5/Reto_5.java
@@ -2,6 +2,7 @@ package Idarraga_Estupinan.Reto5;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class Reto_5 {
@@ -11,5 +12,9 @@ public class Reto_5 {
         HashSet<Integer> num_aleatorios = new HashSet<>(numeros);
         return (HashSet<Integer>) num_aleatorios.stream().filter(n -> n % 3 != 0).collect(Collectors.toSet());
     }
+    public static TreeSet<Integer> tree(List<Integer> numeros) {
 
+        TreeSet<Integer> num_aleatorios = new TreeSet<>(numeros);
+        return num_aleatorios.stream().filter(n -> n % 5 != 0).collect(Collectors.toSet());
+    }
 }    
