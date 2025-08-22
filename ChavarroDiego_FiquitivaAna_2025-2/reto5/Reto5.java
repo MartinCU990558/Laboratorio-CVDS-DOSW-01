a2019173684280125b499ce5f611bd4b43c32e48
Ana Fiquitiva <ana.fiquitiva-p@mail.escuelaing.edu.co>
2025-08-21 19:24:49 -0500

    Estructura base del Reto5

---
+++ ChavarroDiego_FiquitivaAna_2025-2/reto5/Reto5.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Reto5 {
+    public static Set<Integer> crearHashSet(List<Integer> numeros) {
+        
+        return new HashSet<>();
+    }
+
+    public static Set<Integer> crearTreeSet(List<Integer> numeros) {
+        
+        return new TreeSet<>();
+    }
+
+    public static Set<Integer> unirColecciones(Set<Integer> hashSet, Set<Integer> treeSet) {
+        
+        return new HashSet<>();
+    }
+
+    public static void imprimirArena(Set<Integer> conjunto) {
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
