466bbe97dac9d180a27af4618a76d20b38a89d3d
deisyguzman <deisy.guzman-c@mail.escuelaing.edu.co>
2025-08-21 22:28:33 -0500

    Se corrigió el problema de combinar del reto5

--- Pareja_Ramirez_Guzman/reto5/Reto5.java
+++ Pareja_Ramirez_Guzman/reto5/Reto5.java
@@ -1,25 +1,39 @@
-import java.util.Random;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Reto5 {
 
+    public static Set<Integer> generarHashSet() {
+        Random random = new Random();
+        Set<Integer> hashSet = new HashSet<>();
+        for (int i = 0; i < 10; i++) {
+            hashSet.add(random.nextInt(30) + 1);
+        }
+        return hashSet.stream()
+                .filter(n -> n % 3 != 0)
+                .collect(Collectors.toSet());
+    }
+
     public static Set<Integer> generarTreeSet() {
         Random random = new Random();
         Set<Integer> treeSet = new TreeSet<>();
-
         for (int i = 0; i < 10; i++) {
             treeSet.add(random.nextInt(30) + 1);
         }
-
         return treeSet.stream()
                 .filter(n -> n % 5 != 0)
                 .collect(Collectors.toCollection(TreeSet::new));
     }
 
     public static void main(String[] args) {
+        Set<Integer> hashSet = generarHashSet();
         Set<Integer> treeSet = generarTreeSet();
-        treeSet.forEach(n -> System.out.println("Número en TreeSet: " + n));
+
+        Set<Integer> union = new TreeSet<>();
+        union.addAll(hashSet);
+        union.addAll(treeSet);
+
+        union.forEach(n -> System.out.println("Número en arena: " + n));
     }
 }
+
