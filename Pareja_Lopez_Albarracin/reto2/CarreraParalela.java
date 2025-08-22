cec71671f20ea027ba4a0e62378511abaead7528
Sebastián Albarracín Silva <sebastian.albarracin@mail.escuelaing.edu.co>
2025-08-21 22:46:53 -0500

    carril2

--- Pareja_Lopez_Albarracin/reto2/CarreraParalela.java
+++ Pareja_Lopez_Albarracin/reto2/CarreraParalela.java
@@ -1,9 +1,20 @@
 import java.util.List;
 
 public class CarreraParalela {
+
     public static int minimumNumber(List<Integer> numbers) {
         return numbers.stream().min(Integer::compare).get();
     }
 
+    public static int biggerNumber(List<Integer> numbers) {
+        return numbers.stream().max(Integer::compare).get();
+    }
+
+    public static Resultados combineResults(List<Integer> numbers) {
+        int max = biggerNumber(numbers);
+        int min = minimumNumber(numbers);
+        int cantidad = numbers.size();
+        return new Resultados(max, min, cantidad);
+    }
 }
 
