71dee4cd0e4ce2225e79532cd4187ef87945d194
Ana Fiquitiva <ana.fiquitiva-p@mail.escuelaing.edu.co>
2025-08-21 20:46:41 -0500

    Se implementaron los comandos SALUDAR, DESPEDIR, CANTAR, DANZAR

--- ChavarroDiego_FiquitivaAna_2025-2/reto6/Reto6.java
+++ ChavarroDiego_FiquitivaAna_2025-2/reto6/Reto6.java
@@ -4,12 +4,28 @@ public class Reto6 {
 
     private static Map<String, Runnable> comandos = new HashMap<>();
 
+    static {
+        comandos.put("SALUDAR", () -> System.out.println("La máquina dice: ¡Saludos, viajero del tiempo y del código!"));
+        comandos.put("DESPEDIR", () -> System.out.println("La máquina dice: Que los bits te acompañen, hasta la próxima misión."));
+        comandos.put("CANTAR", () -> System.out.println("La máquina canta: 01010101"));
+        comandos.put("DANZAR", () -> System.out.println("La máquina gira y emite chispas: Girando en modo fiesta."));
+    }
+
     public static void ejecutarComando(String comando) {
-        
-        
+        switch (comando) {
+            case "SALUDAR":
+            case "DESPEDIR":
+            case "CANTAR":
+            case "DANZAR":
+                comandos.get(comando).run();
+                break;
+            default:
+                System.out.println("Comando no reconocido");
+        }
     }
 
     public static void main(String[] args) {
-        
+        ejecutarComando("SALUDAR");
+        ejecutarComando("DANZAR");
     }
 }
