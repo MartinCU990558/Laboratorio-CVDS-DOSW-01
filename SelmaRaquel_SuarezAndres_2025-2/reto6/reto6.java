3b9f48382cd8fc1dca106eb8c59526793b0e9d27
curso <raquel.selma-a@mail.escuelaing.edu.co>
2025-08-21 23:44:34 -0500

    Creación de los otros comando ustilizando el método de switch case

--- SelmaRaquel_SuarezAndres_2025-2/reto6/reto6.java
+++ SelmaRaquel_SuarezAndres_2025-2/reto6/reto6.java
@@ -12,3 +12,13 @@ public class Reto6 {
         registrar("DESPEDIR", () -> System.out.println("La máquina dice: Que los bits te acompañen, hasta la próxima misión."));
         registrar("CANTAR", () -> System.out.println("La máquina canta: 01010101"));
         registrar("DANZAR", () -> System.out.println("La máquina gira y emite chispas: Girando en modo fiesta."));
+
+        registrar("BROMEAR", () -> System.out.println("La máquina ríe: ¿Por qué la RAM rompió con la CPU? Porque necesitaba espacio..."));
+        registrar("GRITAR", () -> System.out.println("La máquina grita: ¡¡¡ALERTA DE STACK OVERFLOW!!!"));
+        registrar("SUSURRAR", () -> System.out.println("La máquina susurra: Shhh... los bugs están dormidos."));
+        registrar("ANALIZAR", () -> System.out.println("La máquina procesa: Analizando datos... resultado: ¡Eres increíble programando!"));
+    }
+
+    private void registrar(String clave, Runnable accion) {
+        comandos.put(clave, accion);
+    }
